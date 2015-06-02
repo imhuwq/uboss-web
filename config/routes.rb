f2bd7fab4cbd093175b6c1de37d096ff8d38affc
dalezhang <17652709@qq.com>
2015-06-02 18:26:50 +0800

    just change gemfile

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   }
 
   namespace :admin do
-    root 'home#index'
+    # root 'home#index'
     resources :users do
     end
     resources :products do
@@ -18,6 +18,8 @@ Rails.application.routes.draw do
     resources :sessions do
       get :active_user,:try_to_reset_password,:reset_password, :on=>:collection
       post :send_reset_password_email, :on=>:collection
+    end
+  end
   authenticate :user, lambda { |user| user.admin? } do
     namespace :admin do
       root 'dashboard#index'
