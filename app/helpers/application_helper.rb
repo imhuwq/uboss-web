eb8bb3827d65a83757aa1ea2b8fa5738031b8bb1
wenjiangtao <wenjiangtao@outlook.com>
2015-06-02 15:09:03 +0800

    setup simple_form_foundation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  ALERT_DEFAULT = { type: 'secondary', level: 'info' }
+
+  def alert_box(message, opts = {})
+    content_classes = ALERT_DEFAULT.merge(opts).map { |k,v| v}
+
+    content_tag :div, class: "alert-box #{content_classes.join(' ')}" do
+      [
+        message,
+        link_to('&times;'.html_safe, '#', class: 'close')
+      ].join.html_safe
+    end
+  end
 end
