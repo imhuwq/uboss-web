8c1f174991f5cb4c760646fbd8017c12fc7361f0
dalezhang <17652709@qq.com>
2015-06-04 13:45:43 +0800

    migration has changed

--- db/migrate/20150602071955_create_product.rb
+++ db/migrate/20150602071955_create_product.rb
@@ -9,6 +9,12 @@ class CreateProduct < ActiveRecord::Migration
       t.text    :content
       t.boolean :buyer_pay   ,default: true
       t.float   :traffic_expense
+      t.float   :buyer_lv_1,         default: 0
+      t.float   :buyer_lv_2,         default: 0
+      t.float   :buyer_lv_3,         default: 0
+      t.float   :sharer_lv_1,        default: 0
+      t.integer :buyer_present_way,  default: 0
+      t.integer :sharer_present_way, default: 0
     end
   end
 end
