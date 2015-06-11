ce10c21bb625c8c82b5734e9ea026dab2eb7a428
wenjiangtao <wenjiangtao@outlook.com>
2015-06-11 15:44:29 +0800

    clear code

--- app/models/asset_img.rb
+++ app/models/asset_img.rb
@@ -2,8 +2,11 @@
 #矩形的资源，商品图像
 require 'mime/types'
 class AssetImg < ActiveRecord::Base
+
   attr_accessor :uploaded_data
+
   has_many :children, :class_name=>'AssetImg', :foreign_key=>'parent_id'
   belongs_to :resource, :polymorphic => true #指定图片的类型/对象
+
   mount_uploader :avatar, AssetImgAvatarUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150610080248) do
+ActiveRecord::Schema.define(version: 20150611052903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20150610080248) do
     t.string   "thumbnail"
     t.datetime "created_at"
     t.string   "alt"
+    t.string   "url"
   end
 
   create_table "order_charges", force: :cascade do |t|
