7720462282daa96b91c8e7ac5f3c8f725dcdc7e1
dalezhang <17652709@qq.com>
2015-06-12 14:52:27 +0800

    移动设备窗口-字体自适应基础css

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,7 +10,7 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 
 Rails.application.config.assets.precompile += %w(
-         admin.js admin.css login.js login.css
+         admin.js admin.css login.js login.css mobile.css mobile.js
          jcrop/jquery.Jcrop.js jcrop/ajaxfileupload.js jcrop/jquery.Jcrop.css
           )
 
