629ad970f3b18eacdc0919e84853e162272fe099
wenjiangtao <wenjiangtao@outlook.com>
2015-06-23 13:05:11 +0800

    decrease_product_stock after order item create

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -7,4 +7,12 @@ class OrderItem < ActiveRecord::Base
 
   delegate :name, to: :product, prefix: true
 
+  after_create :decrease_product_stock
+
+  private
+
+  def decrease_product_stock
+    Product.update_counters(product_id, count: -1)
+  end
+
 end
---
+++ test/forms/order_form_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class OrderFormTest < ActiveSupport::TestCase
+
+  test 'should create user if none' do
+
+  end
+
+end
