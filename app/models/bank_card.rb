bc7db63a1bb8b3caa649205a4d6281f084978cbd
wenjiangtao <wenjiangtao@outlook.com>
2015-06-24 17:11:31 +0800

    validate bank_card's bank_name

--- app/models/bank_card.rb
+++ app/models/bank_card.rb
@@ -2,6 +2,6 @@ class BankCard < ActiveRecord::Base
 
   belongs_to :user
 
-  validates :number, :name, :user_id, presence: true
+  validates :number, :name, :user_id, :bank_name, presence: true
 
 end
