46280e72a61028edc539134f481b43a7e4792254
wenjiangtao <wenjiangtao@outlook.com>
2015-06-24 10:36:27 +0800

    test order close

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class OrderTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  describe 'order closed' do
+    it 'should recover sku strock' do
+      buy_amount = 10
+      product = create(:product, count: 100)
+      buyer = create(:user)
+
+      order = create(:order,
+                      user: buyer,
+                      order_items_attributes: [{
+                        product: product,
+                        user: buyer,
+                        amount: buy_amount
+                      }]
+                     )
+
+      assert_equal 90, product.reload.count
+
+      order.close!
+
+      assert_equal 100, product.reload.count
+    end
+  end
 end
