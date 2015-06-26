0c48091a320656e46ef432a6284269ea9f6f297b
wenjiangtao <wenjiangtao@outlook.com>
2015-06-26 15:18:21 +0800

    limit sharing rate

--- app/models/sharing_node.rb
+++ app/models/sharing_node.rb
@@ -10,10 +10,17 @@ class SharingNode < ActiveRecord::Base
   # NOTE also using databse uniq index
   validates_uniqueness_of :user_id, scope: [:product_id, :parent_id]
   validates_uniqueness_of :user_id, scope: [:product_id], if: -> { self.parent_id.blank? }
+  validate :limit_sharing_rate
 
   before_create :set_code, :set_product
 
   private
+  def limit_sharing_rate
+    if self.class.where('created_at > ?', Time.now.beginning_of_day).where(user_id: user_id).exists?
+      errors[:base] << '您今天已经分享，UBoss限制每天分享一件商品，感谢您的支持。'
+    end
+  end
+
   def set_product
     if parent_id.present?
       self.product_id = parent.product_id
