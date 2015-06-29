dcb8cd38a696603bd99820f4ee0593d9ca0ab0ce
wenjiangtao <wenjiangtao@outlook.com>
2015-06-29 15:21:58 +0800

    admin AccessDenied lead to admin_root page

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -5,7 +5,7 @@ class AdminController < ApplicationController
     rescue_from CanCan::AccessDenied do |exception|
       respond_to do |format|
         format.json { render nothing: true, status: :forbidden }
-        format.html { redirect_to root_url, alert: '您无权访问该资源。' }
+        format.html { redirect_to admin_root_url, alert: '您无权访问该资源。' }
       end
     end
   end
