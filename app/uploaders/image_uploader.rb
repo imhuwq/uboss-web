0c8c0598792ac7e828a319ebc66a1bbd33115a56
wenjiangtao <wenjiangtao@outlook.com>
2015-07-08 17:32:53 +0800

    default image & move rewrite_style

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,7 +1,7 @@
 class ImageUploader < CarrierWave::Uploader::Base
 
   def default_url
-    ActionController::Base.helpers.asset_path('/t/noimage.gif')
+    '/t/noimage.gif'
   end
 
   def store_dir
@@ -12,11 +12,7 @@ class ImageUploader < CarrierWave::Uploader::Base
     @url ||= super({})
     version_name = version_name.to_s
     return @url if version_name.blank?
-    # if not version_name.in?(IMAGE_UPLOADER_ALLOW_IMAGE_VERSION_NAMES)
-    #   # To protected version name using, when it not defined, this will be give an error message in development environment
-    #   raise "ImageUploader version_name:#{version_name} not allow."
-    # end
-    [@url, version_name].join("-") # thumb split with "-"
+    [@url, version_name].join("-")
   end
 
   def extension_white_list
