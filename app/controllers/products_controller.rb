d9683893dd0992dcf9755a49ff7b04e2a79c07ab
dalezhang <17652709@qq.com>
2015-07-10 18:28:29 +0800

    debugs

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -31,7 +31,7 @@ class ProductsController < ApplicationController
     end
     @product = Product.find_by_id(params[:id])
     if @product.present? && user.present?
-      @sharing_node = SharingNode.create(user_id: user.id, product_id: @product.id)
+      @sharing_node = SharingNode.find_or_create_by(user_id: user.id, product_id: @product.id)
     end
     respond_to do |format|
       format.html { render nothing: true }
