053ad0eb142b5cb887fc7b99b9f0540176744573
wenjiangtao <wenjiangtao@outlook.com>
2015-07-14 17:09:32 +0800

    init certificate while exists

--- config/initializers/wx_pay.rb
+++ config/initializers/wx_pay.rb
@@ -2,13 +2,15 @@ WxPay.appid = Rails.application.secrets.weixin["app_id"]
 WxPay.key = Rails.application.secrets.weixin["key_secret"]
 WxPay.mch_id = Rails.application.secrets.weixin["app_mchid"]
 
-p12 = OpenSSL::PKCS12.new(
-  File.read(File.join(Rails.root, 'config/apiclient_cert.p12')),
-  WxPay.mch_id
-)
+if File.exists?(File.join(Rails.root, 'config/apiclient_cert.p12'))
+  p12 = OpenSSL::PKCS12.new(
+    File.read(File.join(Rails.root, 'config/apiclient_cert.p12')),
+    WxPay.mch_id
+  )
 
-WxPay.client_cert = p12.certificate
-WxPay.client_key = p12.key
+  WxPay.client_cert = p12.certificate
+  WxPay.client_key = p12.key
+end
 
 # optional - configurations for RestClient timeout, etc.
 WxPay.extra_rest_client_options = {timeout: 10, open_timeout: 10}
