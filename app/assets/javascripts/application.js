b131017cedd60eb5b3c076d345bef8c707abe8d1
dalezhang <17652709@qq.com>
2015-07-19 12:11:58 +0800

    fix account/show active partial bug

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -35,11 +35,11 @@ $(function() {
 		$(".nav_bar").slideToggle();
 	});
 
-  $(".my_menu ul li").click(function(){
-    var Index = $(this).index();
-    $(this).addClass('active').siblings().removeClass('active');
-    $('my_acuntbox2').children('.qre').eq(Index).show().siblings('.qre').hide();
-  });
+  // $(".my_menu ul li").click(function(){
+  //   var Index = $(this).index();
+  //   $(this).addClass('active').siblings().removeClass('active');
+  //   $('my_acuntbox2').children('.qre').eq(Index).show().siblings('.qre').hide();
+  // });
 
 });
 
