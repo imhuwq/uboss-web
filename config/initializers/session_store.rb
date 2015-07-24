08f4bdc54a671988f4ba4291321973037dc842a0
wenjiangtao <wenjiangtao@outlook.com>
2015-07-24 11:02:07 +0800

    use redis store for session on production

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,13 @@
 # Be sure to restart your server when you modify this file.
-
-Rails.application.config.session_store :cookie_store, key: '_UBoss_session'
+if Rails.env.production? || Rails.env.staging?
+  Rails.application.config.session_store :redis_store, servers: {
+    :host => Rails.application.secrets.redis_host,
+    :port => 6379,
+    :db => 0,
+    :password => "mysecret",
+    :namespace => "uboss_session"
+  },
+  :expires_in => 120.minutes
+else
+  Rails.application.config.session_store :cookie_store, key: '_UBoss_session'
+end
