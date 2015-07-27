904b720044f88b1d409f9d7821d1e0b81f48ea52
wenjiangtao <wenjiangtao@outlook.com>
2015-07-27 15:22:40 +0800

    session domain

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,13 +1,14 @@
 # Be sure to restart your server when you modify this file.
 if Rails.env.production? || Rails.env.staging?
   Rails.application.config.session_store :redis_store, servers: {
-    :host => Rails.application.secrets.redis_host,
-    :port => 6379,
-    :db => 0,
-    :password => "mysecret",
-    :namespace => "uboss_session"
+    host: Rails.application.secrets.redis_host,
+    port: 6379,
+    db: 0,
+    password: "mysecret",
+    namespace: "uboss_session",
+    domain: :all,
   },
   :expires_in => 120.minutes
 else
-  Rails.application.config.session_store :cookie_store, key: '_UBoss_session'
+  Rails.application.config.session_store :cookie_store, key: '_UBoss_session', domain: :all
 end
