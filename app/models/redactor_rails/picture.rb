244dc8ab26c46cc27be894744b3226ebcb99d736
wenjiangtao <wenjiangtao@outlook.com>
2015-07-27 11:11:43 +0800

    use w640 version image for product content

--- app/models/redactor_rails/picture.rb
+++ app/models/redactor_rails/picture.rb
@@ -1,7 +1,8 @@
 class RedactorRails::Picture < RedactorRails::Asset
   mount_uploader :data, RedactorRailsPictureUploader, mount_on: :data_file_name
 
-  def url_content
-    url(:content)
+  def url(version='')
+    data.url(:w640)
   end
+
 end
