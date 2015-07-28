354c7437a5b7484d136be424174cd36cf50f910c
dalezhang <17652709@qq.com>
2015-07-28 11:13:25 +0800

    seller/show

--- app/controllers/admin/sellers_controller.rb
+++ app/controllers/admin/sellers_controller.rb
@@ -3,4 +3,10 @@ class Admin::SellersController < AdminController
     @sellers = User.where(agent_id: current_user.id).page(params[:page] || 1).per(15)
   end
 
+  def show
+    @seller = User.find_by_id(params[:id])
+    @personal_authentication = PersonalAuthentication.find_by(user_id: @seller.id) || PersonalAuthentication.new
+    @enterprise_authentication = EnterpriseAuthentication.find_by(user_id: @seller.id) || EnterpriseAuthentication.new
+  end
+
 end
