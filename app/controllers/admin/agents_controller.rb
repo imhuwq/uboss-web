5b9c7144b02f5a9b90d4b9937dec8e8359525a18
dalezhang <17652709@qq.com>
2015-07-29 14:27:22 +0800

    small changes

--- app/controllers/admin/agents_controller.rb
+++ app/controllers/admin/agents_controller.rb
@@ -5,6 +5,7 @@ class Admin::AgentsController < AdminController
 
   def show
     @agent = UserRole.find_by(name: 'agent').users.find(params[:id])
+    @withdraw_records = WithdrawRecord.where(user_id: @agent).page(params[:page] || 1)
   end
 
 end
