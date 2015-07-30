11e21394fa13aed44dee6e2837e2c3b441d6c929
wenjiangtao <wenjiangtao@outlook.com>
2015-07-30 13:14:04 +0800

    fix evaluation comment textarea

--- app/controllers/evaluations_controller.rb
+++ app/controllers/evaluations_controller.rb
@@ -1,12 +1,12 @@
 class EvaluationsController < ApplicationController
   def new
     @order_item = OrderItem.find(params[:id])
+    @privilege_card = PrivilegeCard.find_by(user: current_user, product: @order_item.product, actived: true)
     if @order_item.evaluation.present?
       flash[:success] = "您已经评价过了"
       redirect_to order_path(@order_item.order_id)
     else
       @evaluation = Evaluation.new(order_item: @order_item)
-      @privilege_card = PrivilegeCard.find_by(user: current_user, product: @order_item.product, actived: true)
     end
   end
 
