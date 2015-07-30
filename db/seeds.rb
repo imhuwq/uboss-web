62878eae0b6e3d57e810227895cf726073450f5c
wenjiangtao <wenjiangtao@outlook.com>
2015-07-30 15:38:06 +0800

    db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# Create two necessary user_role
-super_role = UserRole.create(name: 'super_admin', display_name: '超级管理员')
-agent_role = UserRole.create(name: 'agent', display_name: '创客')
-seller_role = UserRole.create(name: 'seller', display_name: '商户')
+p "Create 3 necessary user_role"
+super_role = UserRole.find_or_create_by(name: 'super_admin', display_name: '超级管理员')
+agent_role = UserRole.find_or_create_by(name: 'agent', display_name: '创客')
+seller_role = UserRole.find_or_create_by(name: 'seller', display_name: '商户')
 
-# Create UBOSS official account
+p "Create UBOSS official account"
 u1 = User.create(
   login: '13800000000',
   mobile: '13800000000',
