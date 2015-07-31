694a872c4c77f823a55b0b8b6442d687a4d0d245
wenjiangtao <wenjiangtao@outlook.com>
2015-07-31 16:45:59 +0800

    sidekiq production redis config taking

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: "redis://localhost:6379/1", namespace: "ssobu_queues" }
+  config.redis = { url: "redis://#{Rails.application.secrets.redis_host}:6379/1", namespace: "ssobu_queues" }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: "redis://localhost:6379/1", namespace: "ssobu_queues" }
+  config.redis = { url: "redis://#{Rails.application.secrets.redis_host}:6379/1", namespace: "ssobu_queues" }
 end
