23c6b0d5657632dac2f823b03ed39bb8a7316fb2
wenjiangtao <wenjiangtao@outlook.com>
2015-08-03 18:01:17 +0800

    sync error to errbit

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -3,6 +3,7 @@ if Rails.env.production?
     config.api_key = '3b73c8421cf90fccd9b574d237caf97d'
     config.host    = 'errbit.uboss.cc'
     config.port    = 80
+    config.async   = true
     config.secure  = config.port == 443
   end
 elsif Rails.env.staging?
@@ -10,6 +11,7 @@ elsif Rails.env.staging?
     config.api_key = '53ba15ae15505882c00122b48f307dac'
     config.host    = 'errbit.uboss.cc'
     config.port    = 80
+    config.async   = true
     config.secure  = config.port == 443
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -78,7 +78,7 @@ ActiveRecord::Schema.define(version: 20150803070021) do
   create_table "evaluations", force: :cascade do |t|
     t.integer  "buyer_id"
     t.integer  "sharer_id"
-    t.integer  "status",          default: 3
+    t.integer  "status",          default: 0
     t.integer  "order_item_id"
     t.integer  "product_id"
     t.text     "content"
@@ -165,16 +165,6 @@ ActiveRecord::Schema.define(version: 20150803070021) do
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
     t.boolean  "actived",    default: false
-  end
-
-  create_table "product_share_issues", force: :cascade do |t|
-    t.integer  "product_id"
-    t.integer  "buyer_lv_1_id"
-    t.integer  "buyer_lv_2_id"
-    t.integer  "buyer_lv_3_id"
-    t.integer  "sharer_lv_1_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
   create_table "products", force: :cascade do |t|
