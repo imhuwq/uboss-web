b75d01a454bc78735765ccbf85edad7565c294d0
wenjiangtao <wenjiangtao@outlook.com>
2015-08-04 12:26:42 +0800

    errbit config

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,17 +1,13 @@
-if Rails.env.production?
-  Airbrake.configure do |config|
-    config.api_key = '3b73c8421cf90fccd9b574d237caf97d'
-    config.host    = 'errbit.uboss.cc'
-    config.port    = 80
-    config.async   = true
-    config.secure  = config.port == 443
-  end
-elsif Rails.env.staging?
-  Airbrake.configure do |config|
-    config.api_key = '53ba15ae15505882c00122b48f307dac'
-    config.host    = 'errbit.uboss.cc'
-    config.port    = 80
-    config.async   = true
-    config.secure  = config.port == 443
-  end
+require "rake"
+require "airbrake/rake_handler"
+
+Airbrake.configure do |config|
+  api_key        = Rails.env.production? ? '3b73c8421cf90fccd9b574d237caf97d' : '53ba15ae15505882c00122b48f307dac'
+  config.api_key = api_key
+  config.host    = 'errbit.uboss.cc'
+  config.port    = 80
+  config.secure  = config.port == 443
+  config.async   = true
+  config.logger  = Logger.new("log/airbrake.log")
+  config.rescue_rake_exceptions = true
 end
