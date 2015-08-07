29cb1b522be30a7826da4a189703bcccf3180601
wenjiangtao <wenjiangtao@outlook.com>
2015-08-07 19:02:26 +0800

    remove captcha code from new session

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -12,32 +12,25 @@ class Users::SessionsController < Devise::SessionsController
 
   # POST /resource/sign_in
    def create
-     if true#simple_captcha_valid?
-       if sign_in_params[:mobile_auth_code].present?
-         if true#MobileAuthCode.auth_code(sign_in_params[:login], sign_in_params[:mobile_auth_code])
-           self.resource = User.find_or_create_guest_with_session(sign_in_params[:login], session)
-           if resource.persisted?
-             sign_in(resource)
-             respond_with resource, location: after_sign_in_path_for(resource)
-           else
-             flash.now[:error] = resource.errors.full_messages.join('<br/>')
-             render :new
-           end
+     if sign_in_params[:mobile_auth_code].present?
+       if true#MobileAuthCode.auth_code(sign_in_params[:login], sign_in_params[:mobile_auth_code])
+         self.resource = User.find_or_create_guest_with_session(sign_in_params[:login], session)
+         if resource.persisted?
+           sign_in(resource)
+           respond_with resource, location: after_sign_in_path_for(resource)
          else
-           self.resource = resource_class.new(sign_in_params)
-           flash.now[:error] = '手机验证码错误'
+           flash.now[:error] = resource.errors.full_messages.join('<br/>')
            render :new
          end
        else
-        super do |user|
-          user.update_with_oauth_session(session)
-        end
+         self.resource = resource_class.new(sign_in_params)
+         flash.now[:error] = '手机验证码错误'
+         render :new
        end
      else
-       self.resource = resource_class.new(sign_in_params)
-       clean_up_passwords(resource)
-       set_flash_message(:notice, :invalid_captcha) if is_flashing_format?
-       render :new
+       super do |user|
+         user.update_with_oauth_session(session)
+       end
      end
    end
 
