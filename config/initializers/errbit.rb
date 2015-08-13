3b789e2f56bf59ab5bf4fb3afae6dcb165777cf7
wenjiangtao <wenjiangtao@outlook.com>
2015-08-13 11:05:58 +0800

    errbit host

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -4,7 +4,7 @@ require "airbrake/rake_handler"
 Airbrake.configure do |config|
   api_key        = Rails.env.production? ? '3b73c8421cf90fccd9b574d237caf97d' : '53ba15ae15505882c00122b48f307dac'
   config.api_key = api_key
-  config.host    = 'errbit.uboss.cc'
+  config.host    = 'errbit.uboss.me'
   config.port    = 80
   config.secure  = config.port == 443
   config.async   = true
