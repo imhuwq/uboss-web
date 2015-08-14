8fba4843be1572574f07765cb471652cd55b9081
wenjiangtao <wenjiangtao@outlook.com>
2015-08-14 12:20:09 +0800

    none password can opr admin scope

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,8 +2,6 @@ class AdminController < ApplicationController
 
   include SessionsHelper
 
-  before_action :set_password, if: ->{ request.get? && current_user.need_reset_password? }
-
   layout 'admin'
 
   if not Rails.env.development?
@@ -13,15 +11,6 @@ class AdminController < ApplicationController
         format.html { redirect_to admin_root_url, alert: '您无权访问该资源。' }
       end
     end
-  end
-
-  protected
-
-  def set_password
-    flash[:notice] = '继续操作前请设置您的登录密码'
-    flash[:new_password_enabled] = true
-    store_location_for(current_user, request.fullpath)
-    redirect_to set_password_path
   end
 
 end
