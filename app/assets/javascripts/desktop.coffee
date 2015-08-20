1aa28f0e510a804ab9c7e160fe17c84c40d7af3e
dalezhang <17652709@qq.com>
2015-08-20 12:37:07 +0800

    Merge branch 'feature/official-uboss-frontend' into staging

--- app/assets/javascripts/desktop.coffee
+++ app/assets/javascripts/desktop.coffee
@@ -1,4 +1,9 @@
+<<<<<<< HEAD
 #= require desktop/jquery-2.1.4.min
 #= require desktop/wow
+=======
+#= require jquery2
+#= require wow
+>>>>>>> bf808d4... Merge branch 'feature/official-uboss-frontend' into staging
 #= require desktop/selectTab
 #= require_self
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,9 @@ Rails.application.routes.draw do
 
   get 'wxpay/test/:id', to: 'orders#show', as: :test_wxpay
   get 'sharing/:code', to: 'sharing#show', as: :sharing
-  get 'help', to: 'home#help'
+  get 'service_centre_consumer', to: 'home#service_centre_consumer'
+  get 'service_centre_agent', to: 'home#service_centre_agent'
+  get 'service_centre_tutorial', to: 'home#service_centre_tutorial'
 
   post 'mobile_auth_code/create', to: 'mobile_auth_code#create'
 
