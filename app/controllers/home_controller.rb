a2cd3fb4ca5b348378cd2e1cc71545f571ca3e34
wenjiangtao <wenjiangtao@outlook.com>
2015-08-21 18:36:24 +0800

    production_not_release

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,11 @@
 class HomeController < ApplicationController
 
-  detect_device only: [:index,:service_centre_consumer, :service_centre_agent, :service_centre_tutorial]
+  detect_device only: [:index, :service_centre_consumer, :service_centre_agent, :service_centre_tutorial]
 
   def index
-    if !desktop_request?
-      redirect_to products_path
-    end
+    #if !desktop_request?
+      #redirect_to products_path
+    #end
   end
 
   def service_centre_consumer
