486e0bb0b2ab7efe3a7f2f939418cbe1fa620d23
dalezhang <17652709@qq.com>
2015-09-01 18:24:34 +0800

    modify to_s

--- app/models/user_address.rb
+++ app/models/user_address.rb
@@ -10,7 +10,7 @@ class UserAddress < ActiveRecord::Base
     @province = ChinaCity.get(province) rescue province
     @city = ChinaCity.get(city) rescue city
     @area = ChinaCity.get(area) rescue area
-    "#{@province}#{@city}#{@area}#{street}#{building}"
+    "#{@province}#{@city}#{@area}#{building}"
   end
 
 end
