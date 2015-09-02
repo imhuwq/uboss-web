1c8344b7abb6ee8a6051a5c45332f5d20c1d974c
wenjiangtao <wenjiangtao@outlook.com>
2015-09-02 12:02:29 +0800

    remove test actions, save update pwd

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,22 +1,17 @@
 class Api::V1::UsersController < ApiBaseController
 
-  skip_before_action :authenticate_user_from_token!, :authenticate_user!, only: [:index]
-
-  def index
-    @users = User.limit(10)
-  end
-
-  def all
-    @users = User.limit(2)
-    render :index
-  end
-
   def update_password
-    if @user = current_user.update(password: params.fetch(:password))
+    if @user = current_user.update_with_password(password_params)
       head(200)
     else
       render json: {message: @user.errors.full_messages}, status: :failure
     end
+  end
+
+  private
+
+  def password_params
+    params.permit(:current_password, :password, :password_confirmation)
   end
 
 end
