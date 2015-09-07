bfe16228c600faafd94cc56e36912b136131def1
wenjiangtao <wenjiangtao@outlook.com>
2015-09-07 13:32:21 +0800

    clean code

--- app/services/api_error_service.rb
+++ app/services/api_error_service.rb
@@ -4,17 +4,17 @@ class ApiErrorService
   class UnknwonErrorId < RuntimeError; end
 
   DICT = {
-    unauthorized: 401, # 没通过用户认证
-    forbidden: 403, # 权限不够
-    not_found: 404, # 找不到对应资源
-    unprocessable: 422, # 请求参数非法
-    too_many_requests: 429, # 请求频率超出限制
+    unauthorized:               401, # 没通过用户认证
+    forbidden:                  403, # 权限不够
+    not_found:                  404, # 找不到对应资源
+    unprocessable:              422, # 请求参数非法
+    too_many_requests:          429, # 请求频率超出限制
     wrong_username_or_password: 401001, # 用户名密码错误
-    wrong_params: 422001, # 参数错误，或缺少参数
-    validation_failed: 422002, # 模型验证错误
-    captcha_invalid: 422003, # 验证码不匹配或者过期
-    already_binding_mobile: 422004, # 该手机号已绑定
-    mobile_captcha_fail: 424001, # 短信发送失败
+    wrong_params:               422001, # 参数错误，或缺少参数
+    validation_failed:          422002, # 模型验证错误
+    captcha_invalid:            422003, # 验证码不匹配或者过期
+    already_binding_mobile:     422004, # 该手机号已绑定
+    mobile_captcha_fail:        424001, # 短信发送失败
   }
 
   def self.lookup errid
