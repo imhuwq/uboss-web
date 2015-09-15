4c790749c23bd897cb89965b6123b70725631980
dalezhang <17652709@qq.com>
2015-09-15 15:03:55 +0800

    nothing

--- db/migrate/20150915062458_create_product_class_name.rb
+++ db/migrate/20150915062458_create_product_class_name.rb
@@ -1,7 +1,7 @@
 class CreateProductClassName < ActiveRecord::Migration
   def change
     create_table :product_class_names do |t|
-      t.string  :name
+      t.string  :attribute_name
       t.integer :parent_id
       t.timestamps null: false
     end
