7cc76e9651d9010ea87414604dae158113fcc3c6
liuzhenangel <550686383@qq.com>
2015-09-21 12:23:38 +0800

    单元测试

--- test/models/different_area_test.rb
+++ test/models/different_area_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class DifferentAreaTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '至少添加一个省份' do
+    different_area = build(:different_area)
+    assert_not different_area.save
+  end
+
+  test '包含一个省份' do
+    assert create(:different_area, regions: [create(:region)])
+  end
 end
