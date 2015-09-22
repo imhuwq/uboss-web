ffb3210a6614e502196c3ac282e3b5817c7880f2
wenjiangtao <wenjiangtao@outlook.com>
2015-09-22 11:09:51 +0800

    add seller_id to_privilege_card

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150921094724) do
+ActiveRecord::Schema.define(version: 20150922030908) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -231,6 +231,7 @@ ActiveRecord::Schema.define(version: 20150921094724) do
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
     t.boolean  "actived",    default: false
+    t.integer  "seller_id"
   end
 
   create_table "products", force: :cascade do |t|
