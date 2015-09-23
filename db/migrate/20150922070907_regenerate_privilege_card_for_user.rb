d5fef4b0daf343e6f0cd7f6dbb9873d17b2d92cb
wenjiangtao <wenjiangtao@outlook.com>
2015-09-23 10:07:00 +0800

    add info printing in RegeneratePrivilegeCardForUser migration

--- db/migrate/20150922070907_regenerate_privilege_card_for_user.rb
+++ db/migrate/20150922070907_regenerate_privilege_card_for_user.rb
@@ -1,8 +1,13 @@
 class RegeneratePrivilegeCardForUser < ActiveRecord::Migration
-  def change
-    PrivilegeCard.delete_all
-    Order.select(:user_id, :seller_id).group(:user_id, :seller_id).each do |order|
-      PrivilegeCard.create(user_id: order.user_id, seller_id: order.seller_id, actived: true)
+  def up
+    say_with_time "Delete all old PrivilegeCard" do
+      PrivilegeCard.delete_all
+    end
+
+    say_with_time "Regenerate PrivilegeCard with order data" do
+      Order.select(:user_id, :seller_id).group(:user_id, :seller_id).each do |order|
+        PrivilegeCard.create(user_id: order.user_id, seller_id: order.seller_id, actived: true)
+      end
     end
   end
 end
