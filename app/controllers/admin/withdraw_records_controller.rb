c576e53c5cd5965da5af5de3605d5d4e821a042f
wenjiangtao <wenjiangtao@outlook.com>
2015-09-28 17:25:51 +0800

    find_each for withdraw_record download

--- app/controllers/admin/withdraw_records_controller.rb
+++ app/controllers/admin/withdraw_records_controller.rb
@@ -6,18 +6,6 @@ class Admin::WithdrawRecordsController < AdminController
     @withdraw_records = @withdraw_records.order('id DESC').page(param_page)
   end
 
-  def generate_excel
-    excel = Axlsx::Package.new
-    excel.workbook.add_worksheet(:name => "Basic Worksheet") do |sheet|
-      sheet.add_row ["编号", "申请人", "收款人", "收款银行", "收款账号", "申请时间", "金额", "状态"]
-      @withdraw_records.each do |record|
-        sheet.add_row [record.number, record.user_identify, record.card_username, record.target_title, record.target_content.to_s, record.created_at, record.amount, record.state_i18n],
-          :types => [nil, nil, nil, nil, :string, nil, nil, nil]
-      end
-    end
-    send_data excel.to_stream.read, :filename => 'UBOSS提现记录总表.xlsx', :type => 'application/xlsx'
-  end
-
   def show
   end
 
@@ -45,6 +33,18 @@ class Admin::WithdrawRecordsController < AdminController
 
   def close
     change_record_state(:close!)
+  end
+
+  def generate_excel
+    excel = Axlsx::Package.new
+    excel.workbook.add_worksheet(:name => "Basic Worksheet") do |sheet|
+      sheet.add_row ["编号", "申请人", "收款人", "收款银行", "收款账号", "申请时间", "金额", "状态"]
+      @withdraw_records.find_each do |record|
+        sheet.add_row [record.number, record.user_identify, record.card_username, record.target_title, record.target_content.to_s, record.created_at, record.amount, record.state_i18n],
+          :types => [nil, nil, nil, nil, :string, nil, nil, nil]
+      end
+    end
+    send_data excel.to_stream.read, :filename => "UBOSS提现记录总表#{Date.today}.xlsx", :type => 'application/xlsx'
   end
 
   private
