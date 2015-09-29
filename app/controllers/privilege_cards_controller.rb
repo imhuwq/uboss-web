afe8994498073b146e53ec914ee0a2ebd3a02204
wenjiangtao <wenjiangtao@outlook.com>
2015-09-29 12:07:49 +0800

    limit favoured_products in privilege_card show page & redirect_to privilege_card show page after setPrate

--- app/controllers/privilege_cards_controller.rb
+++ app/controllers/privilege_cards_controller.rb
@@ -16,7 +16,7 @@ class PrivilegeCardsController < ApplicationController
   def show
     @privilege_card = current_user.privilege_cards.find(params[:id])
     @seller = @privilege_card.seller
-    @favour_products = current_user.favoured_products.where(user_id: @seller.id)
+    @favour_products = current_user.favoured_products.where(user_id: @seller.id).recent.limit(6)
     set_sharing_link_node
 
     @products = append_default_filter @seller.products.published, order_column: :updated_at
@@ -25,7 +25,7 @@ class PrivilegeCardsController < ApplicationController
 
   def set_privilege_rate
     if current_user.update(params.require(:user).permit(:privilege_rate))
-      redirect_to edit_rate_privilege_cards_path, notice: '设置成功'
+      redirect_to account_path(anchor: 'showpcards'), notice: '设置成功'
     else
       flash.now[:error] = model_errors(current_user).join('<br/>')
       render :edit_rate
