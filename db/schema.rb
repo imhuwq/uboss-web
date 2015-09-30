21237ad45c0c29c107ca4cfe07615a2507599a83
liuzhenangel <550686383@qq.com>
2015-09-30 15:09:11 +0800

    商家创建的快递公司为私有快递信息

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150916071444) do
+ActiveRecord::Schema.define(version: 20150930064500) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,31 @@ ActiveRecord::Schema.define(version: 20150916071444) do
     t.string   "bankname"
   end
 
+  create_table "carriage_templates", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+  end
+
+  create_table "cart_items", force: :cascade do |t|
+    t.integer  "cart_id"
+    t.integer  "product_id"
+    t.integer  "seller_id"
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.integer  "count",           default: 0
+    t.integer  "sharing_node_id"
+  end
+
+  create_table "carts", force: :cascade do |t|
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "carts", ["user_id"], name: "index_carts_on_user_id", using: :btree
+
   create_table "daily_reports", force: :cascade do |t|
     t.date     "day"
     t.decimal  "amount"
@@ -70,6 +95,21 @@ ActiveRecord::Schema.define(version: 20150916071444) do
     t.integer "resource_id"
     t.string  "resource_type"
     t.text    "content"
+  end
+
+  create_table "different_areas", force: :cascade do |t|
+    t.integer  "carriage_template_id"
+    t.integer  "first_item"
+    t.decimal  "carriage"
+    t.integer  "extend_item"
+    t.decimal  "extend_carriage"
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
+
+  create_table "different_areas_regions", id: false, force: :cascade do |t|
+    t.integer "different_area_id"
+    t.integer "region_id"
   end
 
   create_table "divide_incomes", force: :cascade do |t|
@@ -109,6 +149,7 @@ ActiveRecord::Schema.define(version: 20150916071444) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "private_id"
   end
 
   create_table "expresses_users", id: false, force: :cascade do |t|
@@ -175,6 +216,8 @@ ActiveRecord::Schema.define(version: 20150916071444) do
     t.datetime "completed_at"
     t.string   "ship_number"
     t.integer  "express_id"
+    t.string   "to_seller"
+    t.decimal  "ship_price",      default: 0.0
   end
 
   add_index "orders", ["number"], name: "index_orders_on_number", unique: true, using: :btree
@@ -205,30 +248,32 @@ ActiveRecord::Schema.define(version: 20150916071444) do
     t.integer  "user_id"
     t.string   "name"
     t.string   "code"
-    t.decimal  "original_price",     default: 0.0
-    t.decimal  "present_price",      default: 0.0
-    t.integer  "count",              default: 0
-    t.boolean  "buyer_pay",          default: true
-    t.decimal  "traffic_expense",    default: 0.0
+    t.decimal  "original_price",       default: 0.0
+    t.decimal  "present_price",        default: 0.0
+    t.integer  "count",                default: 0
+    t.boolean  "buyer_pay",            default: true
+    t.decimal  "traffic_expense",      default: 0.0
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "has_share_lv",       default: 3
-    t.decimal  "share_amount_total", default: 0.0
-    t.decimal  "share_amount_lv_1",  default: 0.0
-    t.decimal  "share_amount_lv_2",  default: 0.0
-    t.decimal  "share_amount_lv_3",  default: 0.0
-    t.decimal  "share_rate_lv_1",    default: 0.0
-    t.decimal  "share_rate_lv_2",    default: 0.0
-    t.decimal  "share_rate_lv_3",    default: 0.0
-    t.decimal  "share_rate_total",   default: 0.0
-    t.integer  "calculate_way",      default: 0
-    t.integer  "status",             default: 0
-    t.integer  "good_evaluation",    default: 0
-    t.integer  "normal_evaluation",  default: 0
-    t.integer  "bad_evaluation",     default: 0
-    t.decimal  "privilege_amount",   default: 0.0
+    t.integer  "has_share_lv",         default: 3
+    t.decimal  "share_amount_total",   default: 0.0
+    t.decimal  "share_amount_lv_1",    default: 0.0
+    t.decimal  "share_amount_lv_2",    default: 0.0
+    t.decimal  "share_amount_lv_3",    default: 0.0
+    t.decimal  "share_rate_lv_1",      default: 0.0
+    t.decimal  "share_rate_lv_2",      default: 0.0
+    t.decimal  "share_rate_lv_3",      default: 0.0
+    t.decimal  "share_rate_total",     default: 0.0
+    t.integer  "calculate_way",        default: 0
+    t.integer  "status",               default: 0
+    t.integer  "good_evaluation",      default: 0
+    t.integer  "normal_evaluation",    default: 0
+    t.integer  "bad_evaluation",       default: 0
+    t.decimal  "privilege_amount",     default: 0.0
     t.string   "short_description"
-    t.boolean  "hot",                default: false
+    t.boolean  "hot",                  default: false
+    t.integer  "carriage_template_id"
+    t.integer  "transportation_way",   default: 0
   end
 
   create_table "redactor_assets", force: :cascade do |t|
@@ -247,6 +292,12 @@ ActiveRecord::Schema.define(version: 20150916071444) do
 
   add_index "redactor_assets", ["assetable_type", "assetable_id"], name: "idx_redactor_assetable", using: :btree
   add_index "redactor_assets", ["assetable_type", "type", "assetable_id"], name: "idx_redactor_assetable_type", using: :btree
+
+  create_table "regions", force: :cascade do |t|
+    t.string  "name"
+    t.string  "numcode"
+    t.integer "parent_id"
+  end
 
   create_table "selling_incomes", force: :cascade do |t|
     t.datetime "created_at",               null: false
@@ -286,6 +337,7 @@ ActiveRecord::Schema.define(version: 20150916071444) do
     t.integer  "rgt",        null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "seller_id"
   end
 
   add_index "sharing_nodes", ["code"], name: "index_sharing_nodes_on_code", unique: true, using: :btree
@@ -294,6 +346,7 @@ ActiveRecord::Schema.define(version: 20150916071444) do
   add_index "sharing_nodes", ["rgt"], name: "index_sharing_nodes_on_rgt", using: :btree
   add_index "sharing_nodes", ["user_id", "product_id", "parent_id"], name: "index_sharing_nodes_on_user_id_and_product_id_and_parent_id", unique: true, using: :btree
   add_index "sharing_nodes", ["user_id", "product_id"], name: "index_sharing_nodes_on_user_id_and_product_id", unique: true, where: "(parent_id IS NULL)", using: :btree
+  add_index "sharing_nodes", ["user_id", "seller_id"], name: "index_sharing_nodes_on_user_id_and_seller_id", unique: true, where: "(seller_id IS NOT NULL)", using: :btree
 
   create_table "simple_captcha_data", force: :cascade do |t|
     t.string   "key",        limit: 40
@@ -394,8 +447,10 @@ ActiveRecord::Schema.define(version: 20150916071444) do
     t.integer  "agent_id"
     t.integer  "authenticated",          default: 0
     t.integer  "agent_code"
+    t.string   "authentication_token"
   end
 
+  add_index "users", ["authentication_token"], name: "index_users_on_authentication_token", using: :btree
   add_index "users", ["login"], name: "index_users_on_login", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
@@ -418,6 +473,10 @@ ActiveRecord::Schema.define(version: 20150916071444) do
   add_index "withdraw_records", ["number"], name: "index_withdraw_records_on_number", unique: true, using: :btree
 
   add_foreign_key "bank_cards", "users"
+  add_foreign_key "cart_items", "carts"
+  add_foreign_key "cart_items", "products"
+  add_foreign_key "cart_items", "users", column: "seller_id"
+  add_foreign_key "carts", "users"
   add_foreign_key "order_charges", "orders"
   add_foreign_key "order_items", "orders"
   add_foreign_key "order_items", "products"
@@ -434,6 +493,7 @@ ActiveRecord::Schema.define(version: 20150916071444) do
   add_foreign_key "sharing_incomes", "users"
   add_foreign_key "sharing_incomes", "users", column: "seller_id"
   add_foreign_key "sharing_nodes", "products", on_delete: :cascade
+  add_foreign_key "sharing_nodes", "users", column: "seller_id"
   add_foreign_key "sharing_nodes", "users", on_delete: :cascade
   add_foreign_key "transactions", "users"
   add_foreign_key "user_addresses", "users"
