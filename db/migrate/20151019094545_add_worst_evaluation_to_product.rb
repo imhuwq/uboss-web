3fc0691765c77d6e08ffcd07b3faafb5977321d2
liuzhenangel <550686383@qq.com>
2015-10-20 17:13:35 +0800

    评价数据迁移

--- db/migrate/20151019094545_add_worst_evaluation_to_product.rb
+++ db/migrate/20151019094545_add_worst_evaluation_to_product.rb
@@ -1,6 +1,11 @@
 class AddWorstEvaluationToProduct < ActiveRecord::Migration
   def change
     add_column :products, :worst_evaluation, :integer
+    Product.all.each do |product|
+      product.best_evaluation = product.good_evaluation
+      product.good_evaluation = product.normal_evaluation
+      product.save
+    end
     remove_column :products, :normal_evaluation, :integer
   end
 end
