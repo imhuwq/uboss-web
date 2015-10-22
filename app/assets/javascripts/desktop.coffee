90e538c3e1ba410680410c62be6da454662ec0c0
chenyao <chenyao@ulaiber.com>
2015-10-22 18:44:29 +0800

    new_desktop

--- app/assets/javascripts/desktop.coffee
+++ app/assets/javascripts/desktop.coffee
@@ -1,6 +1,8 @@
 #= require jquery
 #= require modernizr-2.6.2.min
 #= require wow
+#= require desktop/jquery.animateNumber.min
+#= require desktop/main
 #= require selectTab
 #= require_self
 
---
+++ app/assets/javascripts/desktop/main.js
@@ -0,0 +1,48 @@
+$(window).load(function() {
+	if(navigator.appName == "Microsoft Internet Explorer" && navigator.appVersion .split(";")[1].replace(/[ ]/g,"")=="MSIE8.0"){
+
+  	}else{
+    	new WOW().init();
+    }
+  	$('.banner .content').fadeIn()
+})
+$(document).ready(function() {
+  	$('.banner .content').hide()
+	$('#arrow-down').click(function(){		
+		var bh=$(window).height();
+		$('html, body').animate({
+		 	scrollTop: bh
+		},500)
+	})
+	var comma_separator_number_step = $.animateNumber.numberStepFactories.separator(',')
+	$(window).scroll(function(){
+		if($('#number').hasClass('animated')){
+			setTimeout(function(){
+				$('#number span').animateNumber(
+				  {
+				    number: 8349375,
+				    numberStep: comma_separator_number_step
+				  }
+				);
+			},3500)
+			
+		}
+	})
+	
+    $(".video-banner").click(function (){
+    	var isrc=$(this).attr('data-src');
+    	$(".video-bg").fadeIn();
+    	$(".video-box").prepend('<embed class="player" src="'+isrc+'" allowFullScreen="true" quality="high" width="600" height="400" align="middle" allowScriptAccess="always" type="application/x-shockwave-flash"></embed><div class="close"><i class="fa fa-times"></i></div>')
+    });
+    $(".video-box").on("click",".close",function(){
+    	$(".video-bg").fadeOut();
+    	$(".video-box").empty();
+    });
+    $(function(){
+		NumbersAnimate.Target=$(".textC");
+		NumbersAnimate.Numbers=12389623;
+		NumbersAnimate.Duration=1500;
+		NumbersAnimate.Animate();
+	});
+    
+})
