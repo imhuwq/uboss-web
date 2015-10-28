07bf598ff07f8afd9c85c5dede28a4ada1831cf5
wenjiangtao <wenjiangtao@outlook.com>
2015-10-28 16:47:35 +0800

    fix external_id for discourse & refact

--- app/controllers/discourse_sso_controller.rb
+++ app/controllers/discourse_sso_controller.rb
@@ -2,12 +2,12 @@ class DiscourseSsoController < ApplicationController
 
   def sso
     if current_user
-      secret = ENV['SECRET']
-      sso = SingleSignOn.parse(request.query_string, secret)
-      sso.email = current_user.email
-      sso.external_id = ENV['KEY'] # unique to your application
-      sso.sso_secret = secret
-      redirect_to sso.to_url(ENV['CALLBACK_URL'])
+
+      sso = SingleSignOnService.parse(request.query_string)
+      sso.assign_attributes_with_user(current_user)
+
+      Rails.logger.debug("Redirect To Discourse With Playload: #{sso.unsigned_payload}")
+      redirect_to sso.to_url
     else
       redirect_to(new_user_session_url(redirect: 'discourse'))
     end
