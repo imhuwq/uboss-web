52b64c864249d1390759a955b1d4d3917c17e7ba
dalezhang <17652709@qq.com>
2015-10-29 12:05:53 +0800

    disable agent autheration right

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -44,10 +44,6 @@ class Ability
     can :read, User, id: user.id
     can :manage, Order, seller_id: user.id
     can :manage, Product, user_id: user.id
-    #
-    # FIXME manage 已经代表可以做任何操作，重新定义change_status是毫无意义的
-    # can :change_status, Product, user_id: user.id
-    #
     can :manage, PersonalAuthentication, user_id: user.id
     can :manage, EnterpriseAuthentication, user_id: user.id
     can :read,   WithdrawRecord, user_id: user.id
@@ -67,10 +63,14 @@ class Ability
     can :read, User, agent_id: user.id
     can :read, DailyReport, user: { agent_id: user.id }
     can :read, SellingIncome, user: { agent_id: user.id }
+    # 暂时取消agent的认证权利
+    # can :manage, PersonalAuthentication, user_id: user.id
+    # can :manage, EnterpriseAuthentication, user_id: user.id
     can :read, DivideIncome, user_id: user.id
     can :read,   WithdrawRecord, user_id: user.id
     can :create, WithdrawRecord, user_id: user.id
     # FIXME @dalezhang read sellers == read user，这样定义如何判断只能查看自己的商家？？
+    # @yijie 获取的方式是 user.sellers,结果一定是跟自己绑定过的商家，已测试，无法访问没跟当前用户绑定的seller
     can :read, :sellers
     can :manage, BankCard, user_id: user.id
   end
