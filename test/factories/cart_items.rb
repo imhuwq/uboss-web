cfe33de9333cded451ef3529da47f15d29052ec6
Shen.zc <jeffsonit@gmail.com>
2015-11-02 05:52:31 +0800

    add shopping cart test

--- test/factories/cart_items.rb
+++ test/factories/cart_items.rb
@@ -1,9 +1,8 @@
 FactoryGirl.define do
   factory :cart_item do
-    cart_id '1'
-    seller_id '1'
-    count '1'
-    sharing_node_id 1
-    product_inventory_id 1
+    cart
+    count 1
+    seller { create(:user) }
+    product_inventory { create(:product_inventory, product: create(:product, user: seller)) }
   end
 end
