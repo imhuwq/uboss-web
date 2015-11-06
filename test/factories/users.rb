cee85467d2420da8fddc3d0a46b2642c7c400b21
wenjiangtao <wenjiangtao@outlook.com>
2015-11-06 13:09:47 +0800

    test ChargeService process_paid_result & available_pay API

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -4,6 +4,24 @@ FactoryGirl.define do
     sequence(:login) { |n| "18912345#{rand(100)}#{n%10}" }
     password "superPassword"
 
+    trait :agent do
+      after(:create) do |user|
+        create(:agent_role_relation, user: user)
+      end
+    end
+
+    trait :seller do
+      after(:create) do |user|
+        create(:seller_role_relation, user: user)
+      end
+    end
+
+    trait :admin do
+      after(:create) do |user|
+        create(:admin_role_relation, user: user)
+      end
+    end
+
     factory :user_with_address do
       after(:create) do |user|
         create(:user_address, user: user, default: true)
@@ -16,6 +34,11 @@ FactoryGirl.define do
         user.user_info.save
       end
     end
+
+    factory :agent_user, traits: [:agent]
+    factory :admin_user, traits: [:admin]
+    factory :seller_user, traits: [:seller]
+
   end
 
 end
