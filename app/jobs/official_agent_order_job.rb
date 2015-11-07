4d65cd29f361a8bec6b0d51364afb448ce75257f
wenjiangtao <wenjiangtao@outlook.com>
2015-11-08 00:11:45 +0800

    refact OfficialAgentOrderJob

--- app/jobs/official_agent_order_job.rb
+++ app/jobs/official_agent_order_job.rb
@@ -2,18 +2,18 @@ class OfficialAgentOrderJob < ActiveJob::Base
   queue_as :agent_orders
 
   def perform(order)
-    if order.official_agent?
-      user = order.user
+    return false if !order.official_agent?
 
-      User.transaction do
-        user.update_attribute(:admin, true)
-        if not user.user_roles.exists?(name: 'agent')
-          user.user_roles << UserRole.agent
-        end
+    user = order.user
 
-        if order.order_items.size == 1 && order.may_sign?
-          order.sign!
-        end
+    User.transaction do
+      user.update_attribute(:admin, true)
+      if !user.is_agent?
+        user.user_roles << UserRole.agent
+      end
+
+      if order.order_items.size == 1 && order.may_sign?
+        order.sign!
       end
     end
   end
