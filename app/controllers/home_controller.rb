6ce2a1b41d69e05cbd42dd6be26ae41346fa6d47
Shen.zc <jeffsonit@gmail.com>
2015-11-10 16:20:44 +0800

    change slider page layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 
   detect_device only: [:index, :service_centre_consumer, :service_centre_agent, :service_centre_tutorial]
 
-  layout :detect_layout, only: [:index, :service_centre_tutorial, :service_centre_agent, :service_centre_consumer]
+  layout :detect_layout, only: [:index, :service_centre_tutorial, :service_centre_agent, :service_centre_consumer, :lady, :maca, :snacks]
 
   def index
   end
