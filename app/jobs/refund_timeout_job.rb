25027c83a1fa78946381224cd8b255b0406db841
liuzhenangel <550686383@qq.com>
2015-11-11 14:43:25 +0800

    过期时间处理

--- app/jobs/refund_timeout_job.rb
+++ app/jobs/refund_timeout_job.rb
@@ -7,23 +7,24 @@ class RefundTimeoutJob < ActiveJob::Base
   end
 
   private
+
   def seller_timeout
-    refund_timeout_2 = Rails.application.secrets.refund_timeout_2_days
-    refund_timeout_5 = Rails.application.secrets.refund_timeout_5_days
-    refund_timeout_10 = Rails.application.secrets.refund_timeout_10_days
+    refund_timeout_2_days = Rails.application.secrets.refund_timeout['days_2']
+    refund_timeout_5_days = Rails.application.secrets.refund_timeout['days_5']
+    refund_timeout_10_days = Rails.application.secrets.refund_timeout['days_10']
 
     # 未发货 (退款)
-    OrderItemRefund.where(aasm_state: 'pending', refund_type: 'refund')
-      .where("updated_at >= :date", date: Time.now - refund_timeout_2.days)
-      .each do |refund|
-      refund.may_approve? && refund.approve! && refund.create_timeout_message(refund_timeout_2)
-    end
+    @refunds_2_days = OrderItemRefund.where(aasm_state: 'pending', refund_type: 'refund').where("updated_at >= :date", date: Time.now - refund_timeout_2_days.days)
 
     # 已收到货 (已收到货退款 / 未收到货退款 / 售后退款)
-    OrderItemRefund.where(aasm_state: 'pending', refund_type: [:receipted_refund, :unreceipt_refund, :after_sale_only_refund])
-      .where("updated_at >= :date", date: Time.now - refund_timeout_5.days)
-      .each do |refund|
-      refund.may_approve? && refund.approve! && refund.create_timeout_message(refund_timeout_5)
+    @refunds_5_days = OrderItemRefund.where(aasm_state: 'pending', refund_type: ['receipted_refund', 'unreceipt_refund', 'after_sale_only_refund']).where("updated_at >= :date", date: Time.now - refund_timeout_5_days.days)
+
+    @refunds_2_days.each do |refund|
+      refund.may_approve? && refund.approve! && refund.create_timeout_message(refund_timeout_2_days)
+    end
+
+    @refunds_5_days.each do |refund|
+      refund.may_approve? && refund.approve! && refund.create_timeout_message(refund_timeout_5_days)
     end
 
     # 已收到货 (退款退货 / 售后退货)
@@ -34,16 +35,15 @@ class RefundTimeoutJob < ActiveJob::Base
       # TODO 发送退货地址信息 refund_message
     end
 
-    # 卖家收货
-    OrderItemRefund.where(aasm_state: 'completed_express_number')
-      .where("updated_at >= :date", date: Time.now - refund_timeout_10.days)
-      .each do |refund|
+    #买家退货申请处理
+    OrderItemRefund.where(aasm_state: [:completed_express_number], refund_type: 'return_goods_and_refund').where("updated_at >= :date", date: Time.now - refund_timeout_10_days.days).each do |refund|
       refund.may_confirm_receive? && refund.confirm_receive! && refund.create_timeout_message(refund_timeout_10)
     end
   end
 
   def buyer_timeout
-    refund_timeout_7 = Rails.application.secrets.refund_timeout_7_days
+    refund_timeout = Rails.application.secrets.refund_timeout['days_7']
+    @refunds = OrderItemRefund.where(aasm_state: [:approved, :declined, :decline_received]).where("updated_at >= :date", date: Time.now - refund_timeout.days)
 
     # 等待买家回应：卖家拒绝退款申请/卖家拒绝退货申请/卖家拒绝确认收货
     # 等待买家退货
--- app/models/order_item_refund.rb
+++ app/models/order_item_refund.rb
@@ -6,7 +6,9 @@ class OrderItemRefund < ActiveRecord::Base
   has_many :refund_messages
   has_many :asset_imgs, class_name: 'AssetImg', autosave: true, as: :resource
 
-  validates :order_state, presence: true, uniqueness: true
+  validates :order_state, :money, :refund_reason_id, presence: true
+  validates_uniqueness_of :order_state, scope: :order_item_id
+
   after_create :save_state_at_attributes
   after_create  :set_order_item_state
 
