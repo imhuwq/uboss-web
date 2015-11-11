972df9ff2045999089e39cfc656581ff0e8f3a49
Shen.zc <jeffsonit@gmail.com>
2015-11-11 16:46:52 +0800

    refund timeout job

--- app/jobs/refund_timeout_job.rb
+++ app/jobs/refund_timeout_job.rb
@@ -8,27 +8,48 @@ class RefundTimeoutJob < ActiveJob::Base
 
   private
   def seller_timeout
-    refund_timeout = Rails.application.secrets.refund_timeout
-    #退款申请处理
-    @refunds = OrderItemRefund.where(aasm_state: 'pending', refund_type: 'refund').where("updated_at >= :date", date: Time.now - refund_timeout.days)
-    @refunds.each do |refund|
-      refund.may_approve? && refund.approve!
+    refund_timeout_2 = Rails.application.secrets.refund_timeout_2_days
+    refund_timeout_5 = Rails.application.secrets.refund_timeout_5_days
+    refund_timeout_10 = Rails.application.secrets.refund_timeout_10_days
+
+    # 未发货 (退款)
+    OrderItemRefund.where(aasm_state: 'pending', refund_type: 'refund')
+      .where("updated_at >= :date", date: Time.now - refund_timeout_2.days)
+      .each do |refund|
+      refund.may_approve? && refund.approve! && refund.create_timeout_message(refund_timeout_2)
     end
 
-    #退货申请处理
-    @sales = OrderItemRefund.where(aasm_state: [:approved, :declined], refund_type: 'return_goods').where("updated_at >= :date", date: Time.now - refund_timeout.days)
+    # 已收到货 (已收到货退款 / 未收到货退款 / 售后退款)
+    OrderItemRefund.where(aasm_state: 'pending', refund_type: [:receipted_refund, :unreceipt_refund, :after_sale_only_refund])
+      .where("updated_at >= :date", date: Time.now - refund_timeout_5.days)
+      .each do |refund|
+      refund.may_approve? && refund.approve! && refund.create_timeout_message(refund_timeout_5)
+    end
 
-    @sales.each do |sale|
-      #TODO 退货地址信息
-      sale.may_approve? && sale.approve!
+    # 已收到货 (退款退货 / 售后退货)
+    OrderItemRefund.where(aasm_state: 'pending', refund_type: [:return_goods_and_refund, :after_sale_return_goods_and_refund])
+      .where("updated_at >= :date", date: Time.now - refund_timeout_5.days)
+      .each do |refund|
+      refund.may_approve? && refund.approve! && refund.create_timeout_message(refund_timeout_5)
+      # TODO 发送退货地址信息 refund_message
+    end
+
+    # 卖家收货
+    OrderItemRefund.where(aasm_state: 'completed_express_number')
+      .where("updated_at >= :date", date: Time.now - refund_timeout_10.days)
+      .each do |refund|
+      refund.may_confirm_receive? && refund.confirm_receive! && refund.create_timeout_message(refund_timeout_10)
     end
   end
 
   def buyer_timeout
-    refund_timeout = Rails.application.secrets.refund_timeout
-    @refunds = OrderItemRefund.where(aasm_state: [:approved, :declined, :decline_received]).where("updated_at >= :date", date: Time.now - refund_timeout.days)
+    refund_timeout_7 = Rails.application.secrets.refund_timeout_7_days
 
-    @refunds.each do |refund|
+    # 等待买家回应：卖家拒绝退款申请/卖家拒绝退货申请/卖家拒绝确认收货
+    # 等待买家退货
+    OrderItemRefund.where(aasm_state: [:approved, :declined, :decline_received])
+      .where("updated_at >= :date", date: Time.now - refund_timeout_7.days)
+      .each do |refund|
       refund.may_cancel && refund.cancel!
     end
   end
