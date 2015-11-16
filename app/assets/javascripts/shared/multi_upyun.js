251454d57fe782e4798bff51e904af02d6c07337
wenjiangtao <wenjiangtao@outlook.com>
2015-11-16 17:06:31 +0800

    mobile file upload to upyun

--- app/assets/javascripts/shared/multi_upyun.js
+++ app/assets/javascripts/shared/multi_upyun.js
@@ -2,7 +2,7 @@ window.onload=function(){
 	document.getElementById('upload').onclick = function() {
 		console.log(1)
 		var ext =document.getElementById('file').files[0].name.split('.').pop();
-	
+
 		var config = {
 			form_api_url: 'http://v0.api.upyun.com',
 	  	username: 'ulaiberdev',
@@ -10,20 +10,18 @@ window.onload=function(){
 	  	bucket: 'ssobu-dev',
 	  	bucket_key: 'vaQU6JGHQC8HamRHEeT9izlhHqE=',
 	  	bucket_host: 'http://ssobu-dev.b0.upaiyun.com'
-			
-			
 		};
-	
+
 		var instance = new Sand(config);
 		var options = {
 			'notify_url': 'http://ssobu-dev.b0.upaiyun.com'
 		};
-	
+
 		instance.setOptions(options);
 		instance.upload('/asset_img/avatar/' + ext);
 	};
-	
-	
+
+
 		// demo stuff
 	function addLog(data) {
 	  var elem = document.createElement("ul");
@@ -33,12 +31,12 @@ window.onload=function(){
 	  	} else {
 	  		elem.innerHTML += '<li><strong>' + key + ':</strong>' + data[key] + '</li>';
 	  	}
-	
+
 	  }
 	  log.appendChild(elem);
 	  console.log(2)
 	}
-	
+
 	document.addEventListener('uploaded', function(e) {
 		var log = document.getElementById("log");
 		addLog(e.detail);
