6ce959ef1de2308d404fe8a760370c2524cb524b
liuzhenangel <550686383@qq.com>
2015-11-23 17:31:01 +0800

    退款单元测试

--- test/factories/refund_reasons.rb
+++ test/factories/refund_reasons.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :refund_reason do
     reason "MyString"
+    reason_type 'refund'
   end
 
 end
--- test/models/order_item_refund_test.rb
+++ test/models/order_item_refund_test.rb
@@ -1,7 +1,54 @@
 require 'test_helper'
 
 class OrderItemRefundTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'money必须大于0' do
+    refund = build(:order_item_refund, money: 0)
+    assert_not refund.save
+  end
+
+
+  test '未发货前退款,卖家同意' do
+    refund = create(:order_item_refund)
+    refund.approve!
+    assert refund.approved?
+  end
+
+  test '未发货前退款,卖家拒绝' do
+    refund = create(:order_item_refund)
+    refund.decline!
+    assert refund.declined?
+
+    refund.repending!
+    assert refund.may_decline?
+  end
+
+  test '发货后未收到货, 退款' do
+    refund = create(:order_item_refund, refund_type: 'unreceipt_refund')
+    refund.decline!
+    assert refund.declined?
+
+    refund.repending!
+    assert refund.may_approve?
+    assert refund.approve!
+  end
+
+  test '发货后已收到货, 仅退款' do
+    refund = create(:order_item_refund, refund_type: 'receipted_refund')
+    refund.decline!
+    assert refund.declined?
+
+    refund.repending!
+    assert refund.may_approve?
+    assert refund.approve!
+  end
+
+  test '退货退款' do
+    refund = create(:order_item_refund, refund_type: 'return_goods_and_refund')
+    assert refund.approve!
+    assert refund.complete_express_number!
+    assert refund.may_confirm_receive? && refund.may_decline_receive?
+    assert refund.decline_receive!
+    assert refund.complete_express_number!
+    assert refund.confirm_receive!
+  end
 end
