79a41cdeb5eb807f4b26ab5476f6cb8ff5205d54
liuzhenangel <550686383@qq.com>
2015-11-24 11:24:28 +0800

    remove privete

--- app/models/sales_return.rb
+++ app/models/sales_return.rb
@@ -2,7 +2,6 @@ class SalesReturn < ActiveRecord::Base
   belongs_to :order_item_refund
   has_many :asset_imgs, class_name: 'AssetImg', autosave: true, as: :resource
 
-  private
   def image_files
     self.asset_imgs.map{ |img| img.avatar.file.filename }.join(',')
   end
