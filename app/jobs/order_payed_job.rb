15a234580280b91139e892e17a0ead6c1df84d5f
wenjiangtao <wenjiangtao@outlook.com>
2015-11-25 14:10:18 +0800

    refact order payed processing

---
+++ app/jobs/order_payed_job.rb
@@ -0,0 +1,39 @@
+class OrderPayedJob < ActiveJob::Base
+
+  class OrderNotPayed < StandardError; ;end
+
+  queue_as :default
+
+  attr_reader :order, :seller, :buyer, :agent
+
+  def perform(order)
+    @order = order
+    @seller = order.seller
+    @buyer = order.user
+    @agent = @seller && @seller.agent
+    raise OrderNotPayed, order if !order.payed?
+
+    create_privilege_card_if_none
+    send_payed_sms_to_seller
+    send_payed_wx_template_msg
+  end
+
+  private
+
+  def create_privilege_card_if_none
+    order.order_items.each(&:create_privilege_card_if_none)
+  end
+
+  def send_payed_sms_to_seller
+    if seller
+      PostMan.send_sms(seller.login, {name: seller.identify}, 968369)
+    end
+  end
+
+  def send_payed_wx_template_msg
+    WxTemplateMsg.order_payed_msg_to_seller(seller.weixin_openid, self) if seller && seller.weixin_openid.present?
+    WxTemplateMsg.order_payed_msg_to_buyer(buyer.weixin_openid,   self) if buyer  && buyer.weixin_openid.present?
+    WxTemplateMsg.order_payed_msg_to_agent(agent.weixin_openid,   self) if agent  && agent.weixin_openid.present?
+  end
+
+end
