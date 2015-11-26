e9e517654e7c34c1eb66fe7f41f4ddc192bfea7b
liuzhenangel <550686383@qq.com>
2015-11-26 12:29:20 +0800

    退款

--- app/jobs/refund_timeout_job.rb
+++ app/jobs/refund_timeout_job.rb
@@ -15,18 +15,18 @@ class RefundTimeoutJob < ActiveJob::Base
 
     # 未发货 (退款)
     @refunds = OrderItemRefund.where(aasm_state: :pending, refund_type: :refund)
-      .where("deal_at >= :date", date: Time.now - refund_timeout_2_days.days)
+      .where("deal_at <= :date", date: Time.now - refund_timeout_2_days.days)
 
     # 已收到货 (已收到货退款 / 未收到货退款 / 售后退款 / 退款退货 / 售后退货)
     @refunds += OrderItemRefund.where(aasm_state: :pending)
       .where.not(refund_type: :refund)
-      .where("deal_at >= :date", date: Time.now - refund_timeout_5_days.days)
+      .where("deal_at <= :date", date: Time.now - refund_timeout_5_days.days)
 
     @refunds.each { |refund| refund.timeout_approve }
 
     # 买家填写完快递信息
     OrderItemRefund.where(aasm_state: :completed_express_number)
-      .where("deal_at >= :date", date: Time.now - refund_timeout_10_days.days)
+      .where("deal_at <= :date", date: Time.now - refund_timeout_10_days.days)
       .each { |refund| refund.timeout_confirm_receive }
 
   end
@@ -35,7 +35,7 @@ class RefundTimeoutJob < ActiveJob::Base
     # 等待买家回应：卖家拒绝退款申请/卖家拒绝退货申请/卖家拒绝确认收货
     # 等待买家退货
     OrderItemRefund.where(aasm_state: [:approved, :declined, :decline_received])
-      .where("deal_at >= :date", date: Time.now - Rails.application.secrets.refund_timeout['days_7'].days)
+      .where("deal_at <= :date", date: Time.now - Rails.application.secrets.refund_timeout['days_7'].days)
       .each { |refund| refund.timeout_cancel }
 
   end
