d60967f979d73f9098d39c9c2cbd244502d60395
wenjiangtao <wenjiangtao@outlook.com>
2015-11-30 14:29:08 +0800

    limit bonus_record in one day

--- app/models/bonus_record.rb
+++ app/models/bonus_record.rb
@@ -4,4 +4,14 @@ class BonusRecord < ActiveRecord::Base
 
   validates :amount, money: true
 
+  validate :limit_rate
+
+  private
+
+  def limit_rate
+    if self.class.where('created_at > ?', Time.now.beginning_of_day).where(user_id: user_id).count >= 3
+      errors[:base] << '感谢您的支持，每天红包限领三次。'
+    end
+  end
+
 end
