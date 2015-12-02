b65b698777a8180eee1b141e5c1ed73a5125bf89
wenjiangtao <wenjiangtao@outlook.com>
2015-12-02 15:06:05 +0800

    fix refund query params

--- app/jobs/wx_refund_query_job.rb
+++ app/jobs/wx_refund_query_job.rb
@@ -3,9 +3,10 @@ class WxRefundQueryJob < ActiveJob::Base
 
   def perform
     RefundRecord.where(applied_status: 'SUCCESS', refund_status: [nil, 'PROCESSING'])
-      .where("applied_at <= ? and applied_at >= ?", Time.now - 20.minutes, Time.now - 15.days).find_each(batch_size: 500) do |record|
+      .where("applied_at <= ? and applied_at >= ?", Time.now - 20.minutes, Time.now - 15.days)
+      .find_each(batch_size: 500) do |record|
 
-      res = WxPay::Service.invoke_refundquery({ out_trade_no: record.out_refund_no })
+      res = WxPay::Service.invoke_refundquery({ out_refund_no: record.out_refund_no })
       record.query_count += 1
       record.query_content = res
 
