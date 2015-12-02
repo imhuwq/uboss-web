567f3bfc984941e5c7c85288fa4b110cf895d04e
liuzhenangel <550686383@qq.com>
2015-12-02 15:29:41 +0800

    分享

--- app/models/verify_code.rb
+++ app/models/verify_code.rb
@@ -1,6 +1,11 @@
 class VerifyCode < ActiveRecord::Base
   validates_uniqueness_of :code
 
+  default_scope {order("updated_at desc")}
+
+  scope :today, -> { where(verified: true).where('updated_at BETWEEN ? AND ?', Time.now.beginning_of_day, DateTime.now.end_of_day) }
+  scope :total, -> { where(verified: true) }
+
   def generate_code
     self.code = SecureRandom.random_number(100000000000)
   end
