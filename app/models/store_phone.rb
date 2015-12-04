912a5fa64d1ba5fd09de7430f82f195d2e228fec
liuzhenangel <550686383@qq.com>
2015-12-04 11:05:19 +0800

    校验手机号

--- app/models/store_phone.rb
+++ app/models/store_phone.rb
@@ -1,6 +1,8 @@
 class StorePhone < ActiveRecord::Base
   belongs_to :service_store
 
+  validates :phone_number, length: { is: 11 }, format: {with: /\A1[3|4|5|8][0-9]\d{4,8}\z/, message: '无效的手机号码'}, if: "phone_number.present?"
+
   validate do
     if fixed_line.blank? && phone_number.blank?
       self.errors.add(:fixed_line_or_phone_number, "不能为空")
