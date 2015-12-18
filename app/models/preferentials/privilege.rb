81506d182fa172e1978ef0694357a64d00064605
wenjiangtao <wenjiangtao@outlook.com>
2015-12-18 11:51:23 +0800

    auto set amount if privilege preferentials' none

--- app/models/preferentials/privilege.rb
+++ app/models/preferentials/privilege.rb
@@ -1,6 +1,12 @@
 class Preferentials::Privilege < PreferentialMeasure
 
+  before_validation :set_amount
+
   private
+
+  def set_amount
+    self.amount ||= preferential_item.privilege_card.amount(preferential_item.product_inventory)
+  end
 
   def set_total_amount
     self.total_amount = self.amount * self.preferential_item.total_preferential_count
