7f2525c7cfa8279856d21da7ae2bad57a6b4bd8d
Feuda Nan <feuda.nan@gmail.com>
2015-12-24 16:55:43 +0800

    add management of supplier role

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -136,4 +136,10 @@ class Ability
     can :manage, WechatAccount
   end
 
+  def grant_permissions_to_supplier user
+    can :read, User, id: user.id
+    can :read, User, supplier_id: user.id
+    can :read, :sellers
+  end
+
 end
