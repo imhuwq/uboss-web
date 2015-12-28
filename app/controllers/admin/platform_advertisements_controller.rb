f1a067b4bc796f25431e58a2843174c75212b615
dalezhang <17652709@qq.com>
2015-12-28 11:18:26 +0800

    change mobile store style

--- app/controllers/admin/platform_advertisements_controller.rb
+++ app/controllers/admin/platform_advertisements_controller.rb
@@ -1,18 +1,21 @@
 class Admin::PlatformAdvertisementsController < AdminController
 
-	load_and_authorize_resource
+	authorize_resource
 
 	def index
-		 PlatformAdvertisement.accessible_by(current_ability).order("updated_at DESC").page(params[:page] || 1)
+		 @platform_advertisements = Advertisement.where(platform_advertisement: true).order("updated_at DESC").page(params[:page] || 1)
 	end
 
 	def show
 	end
 
 	def new
+    @platform_advertisement = Advertisement.new
 	end
 
 	def create
+    @platform_advertisement = Advertisement.new(platform_advertisement_params)
+    @platform_advertisement.platform_advertisement = true
 		if @platform_advertisement.save
 			flash[:success] = "创建成功"
 			redirect_to action: :index
@@ -23,9 +26,17 @@ class Admin::PlatformAdvertisementsController < AdminController
 	end
 
 	def edit
+    @platform_advertisement = Advertisement.where(platform_advertisement: true).find(params[:id])
 	end
 
 	def update
+    @platform_advertisement = Advertisement.where(platform_advertisement: true).find(params[:id])
+    if @platform_advertisement.update(platform_advertisement_params)
+      flash[:success] = '修改成功'
+    else
+      flash[:error] = '修改失败'
+    end
+    redirect_to action: :index
 	end
 
 	def change_status
@@ -53,7 +64,7 @@ class Admin::PlatformAdvertisementsController < AdminController
 	private
 	# Never trust parameters from the scary internet, only allow the white list through.
 	def platform_advertisement_params
-		params.require(:platform_advertisement).permit(:advertisement_url,:avatar, :status)
+		params.require(:advertisement).permit(:advertisement_url,:avatar, :status)
 	end
 
 end
