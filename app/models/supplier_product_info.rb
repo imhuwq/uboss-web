69f2ca5fe09b78b15a9c9f0b60edc1e58a696751
Feuda Nan <feuda.nan@gmail.com>
2016-01-07 00:08:36 +0800

    add create and show action for supplier products

--- app/models/supplier_product_info.rb
+++ app/models/supplier_product_info.rb
@@ -2,4 +2,7 @@ class SupplierProductInfo < ActiveRecord::Base
   include Descriptiontable
   belongs_to :product
   belongs_to :supplier, class_name: 'User'
+
+  validates :cost_price, presence: true, numericality: { greater_than: 0 }
+  validates :suggest_price_lower, :suggest_price_upper, numericality: { greater_than: 0, allow_nil: true }
 end
