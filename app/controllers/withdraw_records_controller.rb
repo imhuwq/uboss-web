451868911b520468f7f8444d00d357734d030307
wenjiangtao <wenjiangtao@outlook.com>
2016-01-08 15:26:34 +0800

    withdraw_records with new layout & add bankcard

--- app/controllers/withdraw_records_controller.rb
+++ app/controllers/withdraw_records_controller.rb
@@ -1,24 +1,22 @@
-# 提现
 class WithdrawRecordsController < ApplicationController
-    
+
   def show
-	render layout: 'mobile'
+    render layout: 'mobile'
   end
 
   def new
-    @withdraw_record = WithdrawRecord.new(amount: nil)
+    @withdraw_record = current_user.withdraw_records.new(amount: nil)
     render layout: 'mobile'
   end
 
   def create
     amount = params.require(:withdraw_record).permit(:amount)
-    @withdraw_record = WithdrawRecord.new(amount)
-    @withdraw_record.user = current_user
+    @withdraw_record = current_user.withdraw_records.new(amount)
     if @withdraw_record.save
       redirect_to action: :success, id: @withdraw_record.id
     else
-      flash[:error] = model_errors(@withdraw_record).join('<br/>')
-      render :new
+      flash.now[:error] = model_errors(@withdraw_record).join('<br/>')
+      render :new, layout: 'mobile'
     end
   end
 
