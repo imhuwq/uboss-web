12ed4ce8917e27e16fe65b75c10b17c390dd2bd3
wenjiangtao <wenjiangtao@outlook.com>
2016-01-11 14:37:09 +0800

    fix enterprise_authentication img

--- app/models/enterprise_authentication.rb
+++ app/models/enterprise_authentication.rb
@@ -9,8 +9,8 @@ class EnterpriseAuthentication < Certification
   attr_accessor :mobile_auth_code
 
   mount_uploader :business_license_img, ImageUploader, mount_on: :attachment_1
-  mount_uploader :legal_person_identity_card_end_img, ImageUploader, mount_on: :attachment_2
-  mount_uploader :legal_person_identity_card_front_img, ImageUploader, mount_on: :attachment_3
+  mount_uploader :legal_person_identity_card_front_img, ImageUploader, mount_on: :attachment_2
+  mount_uploader :legal_person_identity_card_end_img, ImageUploader, mount_on: :attachment_3
 
   compatible_with_form_api_images :business_license_img, :legal_person_identity_card_end_img, :legal_person_identity_card_front_img
 
