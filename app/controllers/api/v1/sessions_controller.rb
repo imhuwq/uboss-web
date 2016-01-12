491b0edee6521c6120f9c977593d32dea916ab1b
wenjiangtao <wenjiangtao@outlook.com>
2016-01-12 12:01:34 +0800

    email login

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -16,7 +16,7 @@ class Api::V1::SessionsController < ApiBaseController
       else
         render_error :captcha_invalid, '手机验证码错误'
       end
-    elsif @user = User.find_for_authentication(login: login)
+    elsif @user = User.find_for_database_authentication(login_identifier: login)
       if not @user.valid_password?(params[:password])
         render_error :wrong_username_or_password, '账号或密码错误'
       end
