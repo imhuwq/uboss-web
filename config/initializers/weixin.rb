b49db2b0b656f13f2ff171ace3943178946f866a
wenjiangtao <wenjiangtao@outlook.com>
2016-01-13 17:54:07 +0800

    refact hard code weixin menu

--- config/initializers/weixin.rb
+++ config/initializers/weixin.rb
@@ -18,6 +18,24 @@ $weixin_client ||= WeixinAuthorize::Client.new(
 )
 
 if false
+
+  medias = if Rails.env.production?
+             {
+               maker_tutorial: '9EFRB63AS9XXBIcYwxqvKrFP4t16J2-XsP7WeSYUFeM',
+               seller_tutorial: 'mInCvKRGKYG7BQ3NwYoGXEEtnzU2ZBiI9nyXKpZTJF8',
+               contact_tutorial: 'q3Txbg6FYXdnKY7a5cVBzOi9f4-IgzjnKAnKxcoeU1w',
+               sign_in_url: 'http://stage.uboss.me/sign_in',
+               income_url: 'http://stage.uboss.me/account#showincome'
+             }
+           else
+             {
+               maker_tutorial: '64kPaTt2HyTMsJthw6vPbyAlxhhNbTonWrpLPAk3t_U',
+               seller_tutorial: '64kPaTt2HyTMsJthw6vPbyAlxhhNbTonWrpLPAk3t_U',
+               contact_tutorial: '64kPaTt2HyTMsJthw6vPbyAlxhhNbTonWrpLPAk3t_U',
+               sign_in_url: 'http://stage.uboss.me/sign_in',
+               income_url: 'http://stage.uboss.me/account#showincome'
+             }
+           end
   $weixin_client.create_menu(
     button: [
       {
@@ -30,30 +48,25 @@ if false
           {
             name: '创客教程',
             type: 'media_id',
-            #media_id: '9EFRB63AS9XXBIcYwxqvKrFP4t16J2-XsP7WeSYUFeM'
-            media_id: '64kPaTt2HyTMsJthw6vPbyAlxhhNbTonWrpLPAk3t_U'
+            media_id: medias[:maker_tutorial]
           }, {
             name: '商家教程',
             type: 'media_id',
-            #media_id: 'mInCvKRGKYG7BQ3NwYoGXEEtnzU2ZBiI9nyXKpZTJF8'
-            media_id: '64kPaTt2HyTMsJthw6vPbyAlxhhNbTonWrpLPAk3t_U'
+            media_id: medias[:seller_tutorial]
           }, {
             name: '免费入驻',
             type: 'view',
-            #media_id: 'http://uboss.cn/sign_in'
-            media_id: 'http://stage.uboss.me/sign_in'
+            media_id: medias[:sign_in_url]
           }, {
             name: '联系我们',
             type: 'media_id',
-            #media_id: 'q3Txbg6FYXdnKY7a5cVBzOi9f4-IgzjnKAnKxcoeU1w'
-            media_id: '64kPaTt2HyTMsJthw6vPbyAlxhhNbTonWrpLPAk3t_U'
+            media_id: medias[:contact_tutorial]
           }
         ]
       }, {
         name: '我的收益',
         type: 'view',
-        #url: 'http://uboss.cn/account#showincome'
-        url: 'http://stage.uboss.me/account#showincome'
+        url: medias[:income_url]
       }
     ]
   )
