15342507c039068310a31671f3903f08951f2368
plusor <zhoubin@ulaiber.com>
2016-01-20 18:38:42 +0800

    订单"完成"自动触发采购单"完成"

--- app/models/agency_order.rb
+++ app/models/agency_order.rb
@@ -1,7 +1,12 @@
 class AgencyOrder < OrdinaryOrder
+  has_one :purchase_order
   after_create :create_purchase_order
 
   def create_purchase_order
     PurchaseOrder.create(order: self, seller_id: seller_id, supplier_id: 1)
+  end
+
+  def after_completed
+    purchase_order.complete!
   end
 end
