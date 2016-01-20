28c12a4162e01bfd1305db44cfa1a87b3d20fa33
Feuda Nan <feuda.nan@gmail.com>
2016-01-20 18:13:32 +0800

    delete conflict migration

--- db/migrate/20160113063540_add_produce_type_to_products.rb
+++
@@ -1,5 +0,0 @@
-class AddProduceTypeToProducts < ActiveRecord::Migration
-  def change
-    add_column :products, :produce_type, :integer, default: 0
-  end
-end
--- db/migrate/20160115084208_change_produce_type_to_type_in_products.rb
+++
@@ -1,6 +0,0 @@
-class ChangeProduceTypeToTypeInProducts < ActiveRecord::Migration
-  def change
-    remove_column :products, :produce_type, :integer
-    add_column :products, :type, :string
-  end
-end
