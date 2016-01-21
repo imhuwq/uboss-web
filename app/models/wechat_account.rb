b2adae3970b50c7a1ccf04bcdd24b406c6d61dc0
wenjiangtao <wenjiangtao@outlook.com>
2016-01-21 15:20:23 +0800

    uniq WechatAccount weixin_secret_key

--- app/models/wechat_account.rb
+++ app/models/wechat_account.rb
@@ -3,4 +3,6 @@ class WechatAccount < ActiveRecord::Base
   #include WeixinRailsMiddleware::AutoGenerateWeixinTokenSecretKey
 
   validates_presence_of :name
+  validates_uniqueness_of :weixin_secret_key
+
 end
