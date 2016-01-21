38a451d6e341d4e5e50ad224b08b2da06dcd584b
plusor <zhoubin@ulaiber.com>
2016-01-21 18:33:41 +0800

    商品逻辑适用于代销商品

--- app/models/agency_product.rb
+++ app/models/agency_product.rb
@@ -1,4 +1,4 @@
-class AgencyProduct < Product
+class AgencyProduct < OrdinaryProduct
   belongs_to :supplier_product
   belongs_to :supplier, foreign_key: 'supplier_id', class_name: 'User'
   has_many :agency_product_inventories, -> { where(type: 'AgencyProductInventory') }, foreign_key: 'product_id', autosave: true
