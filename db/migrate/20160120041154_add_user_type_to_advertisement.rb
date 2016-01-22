99ebbfac08e63ea83cbb2bfaef4b114c0adb3637
wenjiangtao <wenjiangtao@outlook.com>
2016-01-22 14:48:43 +0800

    improve migrate data

--- db/migrate/20160120041154_add_user_type_to_advertisement.rb
+++ db/migrate/20160120041154_add_user_type_to_advertisement.rb
@@ -2,9 +2,6 @@ class AddUserTypeToAdvertisement < ActiveRecord::Migration
   def change
     add_column :advertisements, :user_type, :string
 
-    Advertisement.all.each do |adv|
-      adv.user_type = 'Ordinary'
-      adv.save
-    end
+    Advertisement.update_all(user_type: 'Ordinary')
   end
 end
