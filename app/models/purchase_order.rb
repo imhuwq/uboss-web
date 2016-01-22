0943e1c75b7085018fd0572f37abe521eeba99d8
plusor <zhoubin@ulaiber.com>
2016-01-22 17:00:00 +0800

    fix 订单拆分根据订单类型拆分后没有对供货商拆分

--- app/models/purchase_order.rb
+++ app/models/purchase_order.rb
@@ -5,12 +5,15 @@ class PurchaseOrder < ActiveRecord::Base
   belongs_to :seller, class_name: 'User'
   has_many :order_items, through: :order
   validates_uniqueness_of :number, allow_blank: true
+  validates :order, presence: true, uniqueness: true
 
   attr_accessor :express_name, :ship_number, :express_id
 
   scope :today, -> (date=Date.today) { where(['DATE(purchase_orders.created_at) = ?', date]) }
   scope :week, -> (time=Time.now) { where(created_at: time.beginning_of_week..time.end_of_week) }
   scope :month, -> (time=Time.now) { where(created_at: time.beginning_of_month..time.end_of_month) }
+
+  before_save :set_default_values, if: :order
 
   enum state: { unpay: 0, payed: 1, shiped: 3, signed: 4, closed: 5, completed: 6 }
 
@@ -53,5 +56,10 @@ class PurchaseOrder < ActiveRecord::Base
     Rails.logger.info e.backtrace.join("\n")
     errors.add(:base, e.message)
     false
+  end
+
+  private
+  def set_default_values
+    assign_attributes(seller_id: order.seller_id, supplier_id: order.supplier_id)
   end
 end
