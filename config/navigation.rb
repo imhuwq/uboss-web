0d3b26314b3b601264b6408bbc1ba14584820739
plusor <zhoubin@ulaiber.com>
2016-01-26 16:49:35 +0800

    市场进货

--- config/navigation.rb
+++ config/navigation.rb
@@ -105,6 +105,16 @@ SimpleNavigation::Configuration.run do |navigation|
         thr_nav.item :edit_store, '店铺设置', admin_store_path(current_user),
           if: -> { can?(:read, Product) }
       end
+      sub_nav.item :fenxiao, '市场进货', "#", {} do |thr_nav|
+        thr_nav.item :new_products, '可代销商品', "/admin/sellers/valid_agent_products",
+          highlights_on: :subpath#, if: -> { can?(:read, Product) }
+
+        thr_nav.item :my_suppliers, '我的供货商', "/admin/sellers/my_suppliers",
+          highlights_on: :subpath#, if: -> { can?(:read, CarriageTemplate) }
+
+        thr_nav.item :explore_products, '发现货源', "/admin/sellers/explore_products",
+          highlights_on: :subpath#, if: -> { can?(:read, Category) }
+      end
     end
 
     primary.item :tuangou, '团购店铺', '#', {} do |sub_nav|
