ced82a1a505f8582dbee04b53481f09f650e02cf
dalezhang <17652709@qq.com>
2016-01-27 15:18:00 +0800

    ReorderProductsJob.perform_now

--- app/jobs/reorder_products_job.rb
+++ app/jobs/reorder_products_job.rb
@@ -5,7 +5,13 @@ class ReorderProductsJob < ActiveJob::Base
   include Loggerable
 
   def perform(user_id)
-    JobHelper.reorder_user_product(user_id) # Rails.root + 'app/helpers/job_helper'
+    logger.info("START: ReorderProducts, user_id: #{user_id}")
+    begin
+      JobHelper.reorder_user_product(user_id) # Rails.root + 'app/helpers/job_helper'
+    rescue => exception
+      logger.error("ERROR: method calling while ReorderProducts, user_id: #{user_id}, exception: #{exception}")
+    end
+    logger.info("DONE: ReorderProducts, user_id: #{user_id}")
   end
 
 end
