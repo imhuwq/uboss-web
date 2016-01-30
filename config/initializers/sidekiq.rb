9ea3824dad4ade55051f0513da6a7b8e7738c735
wenjiangtao <wenjiangtao@outlook.com>
2016-01-30 15:34:36 +0800

    inc Sidekiq pool size

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,17 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: "redis://#{Rails.application.secrets.redis_host}:6379/1", namespace: "ssobu_queues" }
+
+  config.redis = {
+    url: "redis://#{Rails.application.secrets.redis_host}:6379/1",
+    namespace: "ssobu_queues"
+  }
+
+  db_config = ActiveRecord::Base.configurations[Rails.env] ||
+    Rails.application.config.database_configuration[Rails.env]
+  default_pool_size = Rails.env.production? ? 35 : 5
+  db_config['pool'] = ENV['DB_POOL'] || ENV['MAX_THREADS'] || default_pool_size
+
+  ActiveRecord::Base.establish_connection(db_config)
+
 end
 
 Sidekiq.configure_client do |config|
