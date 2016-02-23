2c17728aa2db6eba4cc555c07b9a76a4f231e7be
plusor <zhoubin@ulaiber.com>
2016-02-23 17:54:27 +0800

    fix供货商品规格价格验证

--- app/models/supplier_product_inventory.rb
+++ app/models/supplier_product_inventory.rb
@@ -1,7 +1,7 @@
 class SupplierProductInventory < ProductInventory
 
-  validates_numericality_of :suggest_price_upper, greater_than: ->(spi){ spi.suggest_price_lower }
-  validates_numericality_of :suggest_price_lower, greater_than_or_equal_to: ->(spi){ spi.cost_price }
+  validates_numericality_of :suggest_price_upper, greater_than: :suggest_price_lower,  if: -> { suggest_price_lower.present? }
+  validates_numericality_of :suggest_price_lower, greater_than_or_equal_to: :cost_price, if: -> { cost_price.present? }
 
   belongs_to :supplier_product
   has_many :children, class_name: 'AgencyProductInventory', foreign_key: 'parent_id'
