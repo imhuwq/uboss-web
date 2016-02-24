079f4cff28388b6051b03c45de0997c498f8054d
plusor <zhoubin@ulaiber.com>
2016-02-24 11:09:00 +0800

    supplierProduct factory

--- test/factories/supplier_product_infos.rb
+++ test/factories/supplier_product_infos.rb
@@ -1,7 +1,8 @@
 FactoryGirl.define do
   factory :supplier_product_info do
-    cost_price "9.99"
-suggest_price "9.99"
+    cost_price 100
+    suggest_price_lower 200
+    suggest_price_upper 150
   end
 
 end
