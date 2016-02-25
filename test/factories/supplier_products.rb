0f6df575a173164571434eb22a4e7407add5d7c1
plusor <zhoubin@ulaiber.com>
2016-02-25 11:10:57 +0800

    fix 复制供货商品没有把产品图片复制过去

--- test/factories/supplier_products.rb
+++ test/factories/supplier_products.rb
@@ -18,7 +18,7 @@ FactoryGirl.define do
     association :supplier_product_info, strategy: :build, cost_price: 150, suggest_price_upper: 200, suggest_price_lower: 150.0
     supplier_product_inventories_attributes [
       {
-        price: 100,
+        price: 150,
         suggest_price_lower: 150.0,
         suggest_price_upper: 200.0,
         count: 100,
