ff5772fcb6250021e548ac3a409cd93a165b9075
wenjiangtao <wenjiangtao@outlook.com>
2016-02-26 23:18:46 +0800

    fix product test error

--- test/factories/product.rb
+++ test/factories/product.rb
@@ -15,6 +15,7 @@ FactoryGirl.define do
     transportation_way 1
     traffic_expense 10
     product_inventories_attributes [{ price: 100, count: 100, sku_attributes: { size: 'x', color: 'red' } }]
+    avatar 'fake.jpg'
   end
 
 end
