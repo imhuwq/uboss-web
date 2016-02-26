f17544967540166d466535a869852616467fbac2
liuzhenangel <550686383@qq.com>
2016-02-26 16:57:15 +0800

    fix bug ;

--- app/models/recommend.rb
+++ app/models/recommend.rb
@@ -1,22 +1,10 @@
 class Recommend < ActiveRecord::Base
   belongs_to :user
+  belongs_to :recommended, :polymorphic => true
 
   scope :products, -> {where(recommended_type: ['ServiceProduct', 'OrdinaryProduct'])}
   scope :stores, -> {where(recommended_type: ['ServiceStore', 'OrdinaryStore'])}
 
   validates :recommended_id, :recommended_type, :user_id, presence: true
   validates :user_id, :uniqueness => {:scope => [:recommended_type, :recommended_id]}
-
-  def recommended
-    recommended_type.constantize.find(recommended_id)
-  end
-
-  def recommended= object
-    self.recommended_id = object.id
-    self.recommended_type = object.class
-  end
-
-  private
-  def check_uniq
-  end
 end
