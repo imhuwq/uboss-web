e4924f0dd4dea089dd3d3ed2dfab6c51a9586e2c
Feuda Nan <feuda.nan@gmail.com>
2016-02-29 11:48:43 +0800

    https://hk.tower.im/projects/74d43191827344448a530aeb7fae99f3/todos/e59b846a80c640eb8761895295ec9637/

--- app/models/supplier_product_inventory.rb
+++ app/models/supplier_product_inventory.rb
@@ -3,9 +3,9 @@ class SupplierProductInventory < ProductInventory
   validates_numericality_of :suggest_price_upper, greater_than: :suggest_price_lower,  if: -> { suggest_price_lower.present? }
   validates_numericality_of :suggest_price_lower, greater_than_or_equal_to: :cost_price, if: -> { cost_price.present? }
 
-  belongs_to :supplier_product
+  belongs_to :supplier_product, foreign_key: 'product_id'
   has_many :children, class_name: 'AgencyProductInventory', foreign_key: 'parent_id'
-  after_create :copy_to_children
+  after_create :copy_to_children, if: -> { type == 'SupplierProductInventory' }
   after_destroy :remove_children
 
   amoeba do
@@ -19,7 +19,7 @@ class SupplierProductInventory < ProductInventory
   private
   # TODO 如果数量太多需要放入队列处理
   def copy_to_children
-    product.children.each do |pro|
+    supplier_product.children.each do |pro|
       amoeba_dup.update(product_id: pro.id, user_id: pro.user_id, saling: false)
     end
   end
