d6cdd2a29ff045342cce111918ec4ab1dc7e95c5
Feuda Nan <feuda.nan@gmail.com>
2016-03-02 21:59:58 +0800

    fix https://hk.tower.im/projects/74d43191827344448a530aeb7fae99f3/todos/05150c47a39f41f3b5662d981922c0ae/

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160225090429) do
+ActiveRecord::Schema.define(version: 20160302094519) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -518,7 +518,6 @@ ActiveRecord::Schema.define(version: 20160225090429) do
   end
 
   add_index "products", ["type"], name: "index_products_on_type", using: :btree
-  add_index "products", ["user_id", "parent_id"], name: "index_products_on_user_id_and_parent_id", unique: true, using: :btree
 
   create_table "purchase_orders", force: :cascade do |t|
     t.integer  "seller_id"
