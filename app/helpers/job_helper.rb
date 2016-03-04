8ebf645954e1234e0f45b3b6d42ecd4eb49679f8
dalezhang <17652709@qq.com>
2016-03-04 21:28:29 +0800

    try update updated_at

--- app/helpers/job_helper.rb
+++ app/helpers/job_helper.rb
@@ -16,7 +16,7 @@ module JobHelper
 		          select product_id, 数量排名, Row_Number() over(order by 排名相加) as 综合排名  from T;
 
 
-		      UPDATE products SET comprehensive_order = tb.综合排名, sales_amount_order = tb.数量排名
+          UPDATE products SET comprehensive_order = tb.综合排名, sales_amount_order = tb.数量排名, updated_at = Now()
 		      FROM tb
 		      WHERE products.id = tb.product_id;
 
