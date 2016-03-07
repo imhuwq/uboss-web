d5e2bac35b760d88ba7a4abdb6c36da5f56a98ae
plusor <zhoubin@ulaiber.com>
2016-03-07 16:34:11 +0800

    订单列表分销样式调整

--- app/helpers/admin/orders_helper.rb
+++ app/helpers/admin/orders_helper.rb
@@ -1,7 +1,7 @@
 module Admin::OrdersHelper
   def order_tag(order)
     text = I18n.t(order.type.underscore, scope: 'tags.order')
-    if text
+    if text.present?
       content_tag "span", text, class: "label label-primary"
     end
   end
