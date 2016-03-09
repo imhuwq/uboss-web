e1dda8efbc2da81ce934cee1a2c2053e943cc32f
Shen.zc <jeffsonit@gmail.com>
2016-03-09 14:43:12 +0800

    create or update promotion_activities

--- app/controllers/admin/promotion_activities_controller.rb
+++ app/controllers/admin/promotion_activities_controller.rb
@@ -1,32 +1,44 @@
 class Admin::PromotionActivitiesController < AdminController
-
   load_and_authorize_resource
+
+  before_action :set_activity_info, only: [:new, :edit]
 
   def index
   end
 
-  def new
-    @live_activity_info = @promotion_activity.live_activity_info
-    @share_activity_info = @promotion_activity.share_activity_info
-  end
-
   def create
-    @promotion_activity = PromotionActivity.new(promotion_activity_params)
-    @live_activity_info = @promotion_activity.live_activity_info
-    @share_activity_info = @promotion_activity.share_activity_info
-
-    logger.info promotion_activity_params
-    render :new
+    if @promotion_activity.save
+      flash[:success] = '商家活动创建成功'
+      redirect_to admin_promotion_activities_path
+    else
+      set_activity_info
+      flash.now[:error] = "创建失败。#{@promotion_activity.errors.full_messages.join('<br/>')}"
+      render :new
+    end
   end
 
   def update
+    if @promotion_activity.update(promotion_activity_params)
+      flash[:success] = '修改成功'
+      redirect_to admin_promotion_activities_path
+    else
+      set_activity_info
+      flash.now[:error] = "修改失败。#{@promotion_activity.errors.full_messages.join('<br/>')}"
+      render :edit
+    end
   end
 
   def change_status
   end
 
   private
+  def set_activity_info
+    @live_activity_info  = @promotion_activity.live_activity_info
+    @share_activity_info = @promotion_activity.share_activity_info
+  end
+
   def promotion_activity_params
-    params.require(:promotion_activity).permit(:user_id, activity_infos_attributes: [:activity_type, :name, :price, :expiry_days, :description, :win_count, :win_rate])
+    params.require(:promotion_activity)
+      .permit(:user_id, activity_infos_attributes: [:id, :activity_type, :name, :price, :expiry_days, :description, :win_count, :win_rate])
   end
 end
--- app/models/activity_info.rb
+++ app/models/activity_info.rb
@@ -1,7 +1,7 @@
 class ActivityInfo < ActiveRecord::Base
   belongs_to :promotion_activity
 
-  validates :activity_type, :status, :name, :price, :expiry_days, :win_count, :win_rate, presence: true
+  validates :activity_type, :name, :price, :expiry_days, :win_count, :win_rate, presence: true
   validates :activity_type, inclusion: { in: %w(live share) }
   validates :win_rate, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
   validates :promotion_activity_id, uniqueness: { scope: :activity_type }
