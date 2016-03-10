1e85cdaa8382cbc891c52adfb73932496659dcd6
Shen.zc <jeffsonit@gmail.com>
2016-03-10 12:06:13 +0800

    活动登录页

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,8 +4,6 @@ class HomeController < ApplicationController
 
   layout :detect_layout, only: [:index, :service_centre_tutorial, :service_centre_agent, :service_centre_consumer, :lady, :maca, :snacks,:city]
 
-  before_action :authenticate_user!, only: [:store_qrcode_img]
-
   def index
     if !desktop_request?
       redirect_to stores_path
@@ -44,11 +42,15 @@ class HomeController < ApplicationController
   end
 
   def store_qrcode_img
-    if ['ordinary', 'service'].include?(params[:type]) && (privilege_card = PrivilegeCard.find_or_active_card(current_user.id, params[:sid]))
-      @qrcode_img_url = params[:type] == 'ordinary' ? privilege_card.ordinary_store_qrcode_img_url(true) : privilege_card.service_store_qrcode_img_url(true)
-    end
+    if current_user
+      if ['ordinary', 'service'].include?(params[:type]) && (privilege_card = PrivilegeCard.find_or_active_card(current_user.id, params[:sid]))
+        @qrcode_img_url = params[:type] == 'ordinary' ? privilege_card.ordinary_store_qrcode_img_url(true) : privilege_card.service_store_qrcode_img_url(true)
+      end
 
-    render layout: nil
+      render layout: nil
+    else
+      render 'users/sessions/new_of_activity', layout: 'mobile'
+    end
   end
 
   private
