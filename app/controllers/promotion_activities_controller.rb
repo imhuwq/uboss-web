05d5411ac80b2787337c4e14d0bb8ab3ecfd55ad
dalezhang <17652709@qq.com>
2016-03-21 15:09:52 +0800

    promotion_activities/share_draw

--- app/controllers/promotion_activities_controller.rb
+++ app/controllers/promotion_activities_controller.rb
@@ -39,44 +39,56 @@ class PromotionActivitiesController < ApplicationController
           @message[:error] = "未知错误，请联系管理员。"
         end
         flash[:error] = @message[:error]
-        redirect_to account_verify_codes_path
+        redirect_to lotteries_account_verify_codes_path
         return
       end
     end
   end
 
   def share_draw
-    #@promotion_activity = PromotionActivity.find(params[:id])
-    #@share_activity_info = @promotion_activity.share_activity_info
-    ##@draw_prize = ActivityPrize.find_by(prize_winner_id: current_user.id, promotion_activity_id: @promotion_activity.id, sharer_id: 'sharer_id')
-    #@draw_prize = ActivityPrize.find_by(prize_winner_id: current_user.id, promotion_activity_id: @promotion_activity.id, activity_type: 'live')
+    @promotion_activity = PromotionActivity.find(params[:id])
+    @service_store = @promotion_activity.user.service_store
+    @share_activity_info = @promotion_activity.share_activity_info
+    sharer_id = SharingNode.find_by(code: params[:sharing_node]).try(:user).try(:id)
+    if sharer_id && sharer_id != current_user.id
+      @draw_prize = ActivityPrize.find_by(prize_winner_id: current_user.id,
+                                          promotion_activity_id: @promotion_activity.id,
+                                          sharer_id: sharer_id,
+                                          activity_type: 'share')
+    else
+      flash[:error] = "找不到对应的分享者，请重新获取分享信息"
+      redirect_to lotteries_account_verify_codes_path
+      return
+    end
+    if sharer_id && !@draw_prize.present?
+      @message ||= {}
+      if @share_activity_info.promotion_activity.status == 'published'
+        begin
+          share_activity_prize = @share_activity_info.draw_share_prize(current_user.id, sharer_id)
+          if share_activity_prize.present?
+            @message[:success] = "你中奖了！"
+            @message[:info] = "恭喜，你中奖了！赶快分享二维码给朋友，获得更多中奖机会吧~"
+          else
+            @message[:success] = "没有抽中"
+            @message[:info] = "非常遗憾，你没有抽中。不过分享你的二维码给他人一样有机会中奖哦！"
+          end
+        rescue RepeatedActionError
+          @message[:success] = "已经抽过奖了"
+          @message[:info] = "你已经抽过奖了，分享你的二维码给他人还有机会中奖哦！"
+        end
+      else
+        if @share_activity_info.promotion_activity.status == 'closed'
+          @message[:error] = "活动已过期。"
+        elsif @share_activity_info.promotion_activity.status == 'unpublish'
+          @message[:error] = "活动尚未开始。"
+        else
+          @message[:error] = "未知错误，请联系管理员。"
+        end
+        flash[:error] = @message[:error]
+        redirect_to lotteries_account_verify_codes_path
+        return
+      end
+    end
   end
 
-  def draw_prize
-    activity_info = ActivityInfo.find(params[:activity_info_id])
-    @message ||= {}
-    if activity_info.promotion_activity.status == 'published'
-      begin
-        live_activity_prize = activity_info.draw_live_prize(current_user.id)
-        if live_activity_prize.present?
-          @message[:success] = "你中奖了！"
-          @message[:info] = "恭喜，你中奖了！赶快分享二维码给朋友，获得更多中奖机会吧~"
-        else
-          @message[:success] = "没有抽中"
-          @message[:info] = "非常遗憾，你没有抽中。不过分享你的二维码给他人一样有机会中奖哦！"
-        end
-      rescue RepeatedActionError
-        @message[:success] = "已经抽过奖了"
-        @message[:info] = "你已经抽过奖了，分享你的二维码给他人还有机会中奖哦！"
-      end
-    elsif activity_info.promotion_activity.status == 'closed'
-      @message[:error] = "活动已过期。"
-    elsif activity_info.promotion_activity.status == 'unpublish'
-      @message[:error] = "活动尚未开始。"
-    else
-      @message[:error] = "未知错误，请联系管理员。"
-    end
-    render json: @message.to_json
-    # render json: {success: '你中奖了！', info:  "恭喜，你中奖了！你的分享者--也收到了一份奖品，赶快让他(她)请你吃饭吧:P"}
-  end
 end
