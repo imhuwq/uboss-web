9833234416cb07d2ed10bc2b176777df598402d6
Shen.zc <jeffsonit@gmail.com>
2016-03-28 10:39:01 +0800

    sharing_node

--- app/models/privilege_card.rb
+++ app/models/privilege_card.rb
@@ -50,7 +50,6 @@ class PrivilegeCard < ActiveRecord::Base
 
   def ordinary_store_qrcode_img_url(expire = false)
     if ordinary_store_qrcode_img.new_record? || daily_expired_or_info_charged?(expire)
-      init_sharing_node
       delay.get_and_save_store_qrcode_url
       return "http://imager.ulaiber.com/req/2?#{ordinary_store_qrcode_params.merge(mode: 0).to_param}"
     end
@@ -60,7 +59,6 @@ class PrivilegeCard < ActiveRecord::Base
 
   def service_store_qrcode_img_url(expire = false)
     if service_store_qrcode_img.new_record? || daily_expired_or_info_charged?(expire)
-      init_sharing_node
       delay.get_and_save_store_qrcode_url
       return "http://imager.ulaiber.com/req/2?#{service_store_qrcode_params.merge(mode: 0).to_param}"
     end
@@ -71,12 +69,7 @@ class PrivilegeCard < ActiveRecord::Base
   private
 
   def create_store_qrcode_img
-    init_sharing_node
     delay.get_and_save_store_qrcode_url
-  end
-
-  def init_sharing_node
-    SharingNode.find_or_create_by(user_id: user_id, seller_id: seller_id)
   end
 
   def get_and_save_store_qrcode_url
@@ -165,7 +158,7 @@ class PrivilegeCard < ActiveRecord::Base
   end
 
   def sharing_node
-    @sharing_node ||= SharingNode.find_by(user_id: user_id, seller_id: seller_id)
+    @sharing_node ||= SharingNode.find_or_create_by(user_id: user_id, seller_id: seller_id)
   end
 
   def default_host
