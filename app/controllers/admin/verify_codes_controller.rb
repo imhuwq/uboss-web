5ea82894d2f717bc1deaf26160fe0d6bc7a6dfc1
dalezhang <17652709@qq.com>
2016-03-28 11:28:03 +0800

    fix bug

--- app/controllers/admin/verify_codes_controller.rb
+++ app/controllers/admin/verify_codes_controller.rb
@@ -4,12 +4,12 @@ class Admin::VerifyCodesController < AdminController
   def index
     if params[:type] == 'today'
       @verify_codes = VerifyCode.today(current_user) + VerifyCode.activity_today(current_user)
-      @total = @verify_codes.size
-      @today = VerifyCode.today(current_user).size + VerifyCode.activity_today(current_user).size
-    else
-      @verify_codes = VerifyCode.total(current_user) + VerifyCode.activity_total(current_user)
       @total = VerifyCode.total(current_user).size + VerifyCode.activity_total(current_user).size
       @today = @verify_codes.size
+    else
+      @verify_codes = VerifyCode.total(current_user) + VerifyCode.activity_total(current_user)
+      @total = @verify_codes.size
+      @today = VerifyCode.today(current_user).size + VerifyCode.activity_today(current_user).size
     end
   end
 
