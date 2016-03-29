6c44cb32d31a791cee00f630e83927e82fe04b24
dalezhang <17652709@qq.com>
2016-03-29 17:22:22 +0800

    fix bug

--- app/jobs/expiry_activity_verify_code_job.rb
+++ app/jobs/expiry_activity_verify_code_job.rb
@@ -5,7 +5,7 @@ class ExpiryActivityVerifyCodeJob < ActiveJob::Base
     logger.info("START:ExpiryActivityVerifyCodeJob")
     VerifyCode.where('verified = false and activity_prize_id is not null').each do |obj|
       expiry_days = obj.activity_prize.activity_info.expiry_days
-      if (obj.created_at + expiry_days) > Time.current
+      if (obj.created_at + expiry_days.days) < Time.current
         obj.update(expired: true)
         logger.info("updated #{obj.class}: {id: #{obj.id}, expired: true}")
       end
