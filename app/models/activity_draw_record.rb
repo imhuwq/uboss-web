00183f122e88051194698e2821e3ec718ac7afd7
dalezhang <17652709@qq.com>
2016-04-01 12:29:56 +0800

    code style

--- app/models/activity_draw_record.rb
+++ app/models/activity_draw_record.rb
@@ -3,9 +3,11 @@ class ActivityDrawRecord < ActiveRecord::Base
   belongs_to :activity_info
   belongs_to :user
   belongs_to :sharer, class_name: 'User'
+
   validates :activity_info_id, presence: true
-  before_create :load_info_oncreate
   validates :user_id, uniqueness: { scope: [:sharer_id, :activity_info_id] }
+
+  before_create :load_info_oncreate
 
   def load_info_oncreate
     self.promotion_activity_id = activity_info.promotion_activity_id
