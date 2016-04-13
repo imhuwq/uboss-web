6a6279b0f499e9528abae9b6a53aa2909663f54a
liuzhenangel <550686383@qq.com>
2016-04-13 17:33:15 +0800

    验证详情显示菜品现价

--- app/models/dishes_order.rb
+++ app/models/dishes_order.rb
@@ -2,7 +2,7 @@ class DishesOrder < ServiceOrder
   has_one :verify_code, -> { where(target_type: 'DishesOrder') }, foreign_key: :target_id
   validates :order_items,  presence: true
   def present_price
-    self.order_items.sum(:pay_amount)
+    self.order_items.sum(:present_price)
   end
 
   def verified_time
