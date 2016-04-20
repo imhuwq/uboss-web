60cee49c26f9143bb760644000b9d45e5c730184
wenjiangtao <wenjiangtao@outlook.com>
2016-04-20 18:04:47 +0800

    login in with ajax

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -8,6 +8,8 @@ class Users::SessionsController < Devise::SessionsController
 
   before_filter :configure_sign_in_params, only: [:create]
 
+  respond_to :json, :html
+
   # GET /resource/sign_in
   def new
     if params[:redirect] == "activity"
@@ -28,15 +30,24 @@ class Users::SessionsController < Devise::SessionsController
            sign_in(resource)
            MobileCaptcha.clear_captcha(sign_in_params[:login])
            Ubonus::Invite.delay.active_by_user_id(resource.id)
-           redirect_to after_sign_in_path_for(resource)
+           respond_to do |format|
+             format.html { redirect_to after_sign_in_path_for(resource) }
+             format.json { render json: { user_id: resource.id } }
+           end
          else
            error_msg = resource.errors.full_messages.join('<br/>')
-           render_or_redirect(error_msg, params[:redirect])
+           respond_to do |format|
+             format.html { render_or_redirect(error_msg, params[:redirect]) }
+             format.json { render json: { error: error_msg }, status: 422 }
+           end
          end
        else
          self.resource = resource_class.new(sign_in_params)
          error_msg = '手机验证码错误'
-         render_or_redirect(error_msg, params[:redirect])
+         respond_to do |format|
+           format.html { render_or_redirect(error_msg, params[:redirect]) }
+           format.json { render json: { error: error_msg }, status: 401 }
+         end
        end
      else
        wechat_omniauth_data = { "devise.wechat_data" => session["devise.wechat_data"] }
