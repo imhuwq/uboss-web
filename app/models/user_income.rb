7ba0fea9c2e5d3140d7bf7c99f43380145bbe856
plusor <zhoubin@ulaiber.com>
2016-04-24 21:37:24 +0800

    fix association

--- app/models/user_income.rb
+++ app/models/user_income.rb
@@ -1,4 +1,5 @@
 class UserIncome < ActiveRecord::Base
   include Orderable
+  belongs_to :resource, polymorphic: true
   scope :sales, -> { where(resource_type: %w(SellingIncome BillIncome )) }
 end
